761138d4b286a674df36c7661bc72cf2c267974b
Dmytro Somik <dmytro.somik@mccann.com>
2023-06-28 21:33:44 +0100

    Solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,45 +16,107 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_FIELD_NAME = 'name';
+const SORT_FIELD_LENGTH = 'length';
+const SORT_DIRECTION_ASC = 'asc';
+const SORT_DIRECTION_DESC = 'desc';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function sortGoods(goods, { sortField, sortDirection }) {
+  const localGoods = [...goods];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (!sortField && sortDirection) {
+    return localGoods.reverse();
+  }
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+  localGoods.sort((good1, good2) => {
+    switch (sortField) {
+      default:
+        return 0;
+
+      case SORT_FIELD_NAME:
+        return sortDirection === SORT_DIRECTION_ASC
+          ? good2.localeCompare(good1)
+          : good1.localeCompare(good2);
+
+      case SORT_FIELD_LENGTH:
+        return sortDirection === SORT_DIRECTION_ASC
+          ? good2.length - good1.length
+          : good1.length - good2.length;
+    }
+  });
+
+  return localGoods;
+}
+
+export const App = () => {
+  const [sortField, setSortField] = useState('');
+  const [sortDirection, setSortDirection] = useState('');
+
+  const showGoods = sortGoods(goodsFromServer, { sortField, sortDirection });
+
+  const reset = () => {
+    setSortField('');
+    setSortDirection('');
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn(
+            ['button', 'is-info'],
+            { 'is-light': sortField !== SORT_FIELD_NAME },
+          )}
+          onClick={() => setSortField(SORT_FIELD_NAME)}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={cn(
+            ['button', 'is-success'],
+            { 'is-light': sortField !== SORT_FIELD_LENGTH },
+          )}
+          onClick={() => setSortField(SORT_FIELD_LENGTH)}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={cn(
+            ['button', 'is-warning'],
+            { 'is-light': sortDirection === SORT_DIRECTION_DESC },
+          )}
+          onClick={() => {
+            const direction = sortDirection === SORT_DIRECTION_DESC
+              ? SORT_DIRECTION_ASC
+              : SORT_DIRECTION_DESC;
+
+            setSortDirection(direction);
+          }}
+        >
+          Reverse
+        </button>
+
+        {(sortField || sortDirection) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {showGoods.map(good => (
+          <li data-cy="Good" key={good}>{good}</li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
