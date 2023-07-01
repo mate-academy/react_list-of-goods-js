35a765cb510086d710c2eefe16f4bcb00fa944b7
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-07-01 16:43:23 +0300

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,45 +17,113 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_FIELD_ALPHABET = 'aplhabet';
+const SORT_FIELD_LENGTH = 'length';
+const SORT_FIELD_REVERSE = 'reverse';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function getSortedElements(goods, sortField, sortReverse) {
+  let preparedGoods = [...goods];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (sortField) {
+    switch (sortField) {
+      case SORT_FIELD_ALPHABET:
+        preparedGoods = preparedGoods
+          .sort((good1, good2) => good1.localeCompare(good2));
+        break;
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+      case SORT_FIELD_LENGTH:
+        preparedGoods = preparedGoods
+          .sort((good1, good2) => good1.length - good2.length);
+        break;
+      default:
+        return 0;
+    }
+  }
+
+  if (sortReverse) {
+    preparedGoods = preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
+
+function getButtonColor(sortField, thisSortField) {
+  return sortField !== thisSortField;
+}
+
+function ifReversed(reverseField) {
+  return !reverseField
+    ? SORT_FIELD_REVERSE
+    : '';
+}
+
+export const App = () => {
+  const [sortField, setSortField] = useState('');
+  const [reverseField, setReverseField] = useState('');
+  const visibleGoods = getSortedElements(goodsFromServer,
+    sortField, reverseField);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          onClick={() => setSortField(SORT_FIELD_ALPHABET)}
+          type="button"
+          className={classNames(
+            'button', 'is-info',
+            { 'is-light': getButtonColor(sortField, SORT_FIELD_ALPHABET) },
+          )}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          onClick={() => setSortField(SORT_FIELD_LENGTH)}
+          type="button"
+          className={classNames(
+            'button', 'is-success',
+            { 'is-light': getButtonColor(sortField, SORT_FIELD_LENGTH) },
+          )}
+        >
+          Sort by length
+        </button>
+
+        <button
+          onClick={() => setReverseField(ifReversed(reverseField))}
+          type="button"
+          className={classNames(
+            'button', 'is-warning',
+            { 'is-light': getButtonColor(reverseField, SORT_FIELD_REVERSE) },
+          )}
+        >
+          Reverse
+        </button>
+
+        {(sortField || reverseField) && (
+          <button
+            onClick={() => {
+              setSortField('');
+              setReverseField('');
+            }}
+            type="button"
+            className="button is-danger is-light"
+          >
+            Reset
+          </button>
+        )}
+
+      </div>
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li
+            key={good}
+            data-cy="Good"
+          >
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
