23abb8d1d13d32dc5e13a8c543525eae85f33c0e
Maksym Ptashnyk <super.zet65@gmail.com>
2023-07-04 14:52:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,10 @@ export const App = () => {
   const [sortField, setSortField] = useState('');
   const [reversed, setReversed] = useState(false);
   const visibleGoods = getPreparedGoods(goodsFromServer, sortField, reversed);
-
+  const onReverse = () => {
+    return () => { setReversed(false); setSortField('');}
+  }
+ 
   return (
     <div className="section content">
       <div className="buttons">
@@ -93,9 +96,7 @@ export const App = () => {
             <button
               type="button"
               className="button is-danger is-light"
-              onClick={() => {
-                setReversed(false); setSortField('');
-              }}
+              onClick={onReverse}
             >
               Reset
             </button>
