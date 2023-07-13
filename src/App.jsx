bd27bae5846037b4b2b501553586a512ccb89112
Ilia Osoben <osoben17@gmail.com>
2023-07-13 19:19:28 +0300

    added list_of_goods

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +16,94 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_BY_ALPHA = 'alphabet';
+const SORT_BY_LENGTH = 'length';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function getPrepareGoods(goods, sortGood, reverse) {
+  const prepareGoods = [...goods];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (prepareGoods) {
+    prepareGoods.sort((good1, good2) => {
+      switch (sortGood) {
+        case SORT_BY_ALPHA:
+          return good1.localeCompare(good2);
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
-    </div>
+        case SORT_BY_LENGTH:
+          return good1.length - good2.length;
 
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (reverse) {
+    prepareGoods.reverse();
+  }
+
+  return prepareGoods;
+}
+
+const GoodsList = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li data-cy="Good" key={good}>
+        {good}
+      </li>
+    ))}
+  </ul>
 );
+
+export const App = () => {
+  const [sortGood, setSortGood] = useState('');
+  const [reverse, setReverse] = useState(false);
+  const visibleGoods = getPrepareGoods(goodsFromServer, sortGood, reverse);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          onClick={() => setSortGood(SORT_BY_ALPHA)}
+          type="button"
+          className={cn('button', ' is-info', {
+            'is-light': sortGood !== SORT_BY_ALPHA,
+          })}
+        >
+          Sort alphabetically
+        </button>
+        <button
+          onClick={() => setSortGood(SORT_BY_LENGTH)}
+          type="button"
+          className={cn('button', ' is-success', {
+            'is-light': sortGood !== SORT_BY_LENGTH,
+          })}
+        >
+          Sort by length
+        </button>
+        <button
+          onClick={() => setReverse(!reverse)}
+          type="button"
+          className={cn('button', 'is-warning', {
+            'is-light': reverse !== true,
+          })}
+        >
+          Reverse
+        </button>
+        {(sortGood || reverse) && (
+          <button
+            onClick={() => {
+              setSortGood('');
+              setReverse(false);
+            }}
+            type="button"
+            className="button is-danger is-light"
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <GoodsList goods={visibleGoods} />
+    </div>
+  );
+};
