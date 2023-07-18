6ab546e9fa3536d65d22d8952f3fc121e8f1f626
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-07-18 11:55:22 +0300

    fixed misstake

--- src/App.jsx
+++ src/App.jsx
@@ -111,7 +111,7 @@ export const App = () => {
 
       <ul>
         {goodsForSort.map(good => (
-          <li data-cy="Good">{good}</li>
+          <li data-cy="Good" key={good}>{good}</li>
         ))}
       </ul>
     </div>
