8d5b5edfc795d47f1d249721c8f656b6b47d0ae4
Sergej <sergej161995@gmail.com>
2023-08-04 22:02:06 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { useState } from 'react';
 import cn from 'classnames';
 import './App.scss';
 
+const SORT = {
+  ALPHABETICALLY: 'Sort alphabetically',
+  BY_LENGTH: 'Sort by length',
+  NONE: 'none',
+};
+
 export const goodsFromServer = [
   'Dumplings',
   'Carrot',
@@ -16,95 +22,90 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-const SORT_ALPHABETICALLY = 'Sort alphabetically';
-const SORT_BY_LENGTH = 'Sort by length';
-
-const getPreparedGoods = (goods, { sortField, toReverse }) => {
-  const preparedGoods = [...goods];
-
-  if (sortField) {
-    preparedGoods.sort((good1, good2) => {
-      switch (sortField) {
-        case SORT_ALPHABETICALLY:
-          return good1.localeCompare(good2);
-
-        case SORT_BY_LENGTH:
-          return good1.length - good2.length;
-
-        default:
-          return 0;
-      }
-    });
-  }
-
-  if (toReverse) {
-    preparedGoods.reverse();
-  }
-
-  return preparedGoods;
-};
-
 export const App = () => {
-  const [sortField, setSortField] = useState('');
+  const [sortField, setSortField] = useState(SORT.NONE);
   const [toReverse, setToReverse] = useState(false);
-  const visibleGoods
-    = getPreparedGoods(goodsFromServer, { sortField, toReverse });
+
+  const getPreparedGoods = (goods, { sortField:
+     field, toReverse: isReversed }) => {
+    const preparedGoods = [...goods];
+
+    if (field !== SORT.NONE) {
+      preparedGoods.sort((good1, good2) => {
+        switch (field) {
+          case SORT.ALPHABETICALLY:
+            return good1.localeCompare(good2);
+
+          case SORT.BY_LENGTH:
+            return good1.length - good2.length;
+
+          default:
+            return 0;
+        }
+      });
+    }
+
+    if (isReversed) {
+      preparedGoods.reverse();
+    }
+
+    return preparedGoods;
+  };
+
+  const handleSort = field => () => {
+    setSortField(field);
+  };
+
+  const handleReset = () => {
+    setSortField(SORT.NONE);
+    setToReverse(false);
+  };
+
+  const visibleGoods = getPreparedGoods(goodsFromServer,
+    { sortField, toReverse });
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
-          onClick={() => setSortField(SORT_ALPHABETICALLY)}
+          onClick={handleSort(SORT.ALPHABETICALLY)}
           type="button"
           className={cn('button', 'is-info', {
-            'is-light': sortField !== SORT_ALPHABETICALLY,
+            'is-light': sortField !== SORT.ALPHABETICALLY,
           })}
         >
-          Sort alphabetically
+          {SORT.ALPHABETICALLY}
         </button>
 
         <button
-          onClick={() => setSortField(SORT_BY_LENGTH)}
+          onClick={handleSort(SORT.BY_LENGTH)}
           type="button"
           className={cn('button', 'is-success', {
-            'is-light': sortField !== SORT_BY_LENGTH,
+            'is-light': sortField !== SORT.BY_LENGTH,
           })}
         >
-          Sort by length
+          {SORT.BY_LENGTH}
         </button>
 
         <button
-          onClick={() => {
-            if (toReverse === false) {
-              setToReverse(true);
-            } else {
-              setToReverse(false);
-            }
-          }}
+          onClick={() => setToReverse(!toReverse)}
           type="button"
           className={cn('button', 'is-warning', {
-            'is-light': toReverse === false,
+            'is-light': !toReverse,
           })}
         >
           Reverse
         </button>
 
-        {
-          sortField || toReverse
-            ? (
-              <button
-                onClick={() => {
-                  setSortField('');
-                  setToReverse(false);
-                }}
-                type="button"
-                className="button is-danger is-light"
-              >
-                Reset
-              </button>
-            )
-            : ''
-        }
+        {(sortField !== SORT.NONE || toReverse) && (
+          <button
+            onClick={handleReset}
+            type="button"
+            className="button is-danger is-light"
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
