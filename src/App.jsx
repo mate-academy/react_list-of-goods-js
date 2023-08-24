49df0c74f691b36a2f1cdd7a7f7d54816f970fd3
andrew gidrovich <mr.androvich55555@gmail.com>
2023-08-24 18:18:18 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -17,70 +17,41 @@ const initialGoods = [
 
 export const App = () => {
   const [goods, setGoods] = useState(initialGoods);
-  const [buttonStates, setButtonStates] = useState({
-    sortAlphabetically: true,
-    sortByLength: true,
-    reverse: true,
-    reset: true,
-  });
+  const [sortType, setSortType] = useState('');
   const [ascending, setAscending] = useState(true);
-  const [originalOrder] = useState([...initialGoods]);
-  const [isReversed, setIsReversed] = useState(false);
 
-  const handleButtonClick = (buttonName) => {
-    const newButtonStates = {
-      sortAlphabetically: buttonName === 'sortAlphabetically',
-      sortByLength: buttonName === 'sortByLength',
-      reverse: buttonName === 'reverse',
-      reset: buttonName === 'reset',
-    };
+  const handleSort = (type) => {
+    let sortedGoods = [...goods];
 
-    setButtonStates(newButtonStates);
-  };
+    if (type === 'alphabetically') {
+      sortedGoods = stableSort([...goods], (a, b) => a.localeCompare(b));
+    } else if (type === 'length') {
+      sortedGoods = stableSort(
+        [...goods], (a, b) => a.length - b.length
+        || goods.indexOf(a) - goods.indexOf(b),
+      );
+    }
 
-  const stableSort = (array, compareFunction) => array
-    .map((item, index) => ({ item, index }))
-    .sort((a, b) => compareFunction(a.item, b.item) || a.index - b.index)
-    .map(({ item }) => item);
-
-  const sortAlphabetically = () => {
-    const sortedGoods = stableSort([...goods], (a, b) => a.localeCompare(b));
+    if (!ascending) {
+      sortedGoods.reverse();
+    }
 
     setGoods(sortedGoods);
+    setSortType(type);
     setAscending(true);
-    handleButtonClick('sortAlphabetically');
-    setIsReversed(false);
   };
 
-  const sortByLengthStable = () => {
-    const sortedGoods = stableSort([...goods], (a, b) => {
-      if (a.length !== b.length) {
-        return a.length - b.length;
-      }
-
-      return goods.indexOf(a) - goods.indexOf(b);
-    });
-
-    setGoods(sortedGoods);
-    setAscending(true);
-    handleButtonClick('sortByLength');
-    setIsReversed(false);
+  const handleReverse = () => {
+    if (sortType) {
+      setGoods([...goods].reverse());
+      setAscending(!ascending);
+    }
   };
 
-  const toggleReverseOrder = () => {
-    const newGoods = isReversed ? [...originalOrder] : [...goods].reverse();
-
-    setGoods(newGoods);
-    setAscending(!ascending);
-    handleButtonClick('reverse');
-    setIsReversed(!isReversed);
-  };
-
-  const resetOrder = () => {
-    setGoods([...originalOrder]);
+  const handleReset = () => {
+    setGoods(initialGoods);
+    setSortType('');
     setAscending(true);
-    handleButtonClick('reset');
-    setIsReversed(false);
   };
 
   return (
@@ -88,40 +59,44 @@ export const App = () => {
       <div className="buttons">
         <button
           type="button"
-          className={`button is-info ${buttonStates.sortAlphabetically ? '' : 'is-light'}`}
-          onClick={sortAlphabetically}
+          className={`button is-info ${sortType === 'alphabetically' ? '' : 'is-light'}`}
+          onClick={() => handleSort('alphabetically')}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className={`button is-success ${buttonStates.sortByLength ? '' : 'is-light'}`}
-          onClick={sortByLengthStable}
+          className={`button is-success ${sortType === 'length' ? '' : 'is-light'}`}
+          onClick={() => handleSort('length')}
         >
           Sort by length
         </button>
 
-        <button
-          type="button"
-          className={`button is-warning ${buttonStates.reverse ? '' : 'is-light'}`}
-          onClick={toggleReverseOrder}
-        >
-          Reverse
-        </button>
+        {(sortType || !ascending) && (
+          <button
+            type="button"
+            className="button is-warning"
+            onClick={handleReverse}
+          >
+            Reverse
+          </button>
+        )}
 
-        <button
-          type="button"
-          className={`button is-danger ${buttonStates.reset ? '' : 'is-light'}`}
-          onClick={resetOrder}
-        >
-          Reset
-        </button>
+        {(sortType || !ascending) && (
+          <button
+            type="button"
+            className="button is-danger"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         {goods.map(good => (
-          <li key={good} data-cy="Good">
+          <li key={good} data-cy={good}>
             {good}
           </li>
         ))}
@@ -129,3 +104,9 @@ export const App = () => {
     </div>
   );
 };
+
+// Stable sort implementation
+const stableSort = (array, compareFunction) => array
+  .map((item, index) => ({ item, index }))
+  .sort((a, b) => compareFunction(a.item, b.item) || a.index - b.index)
+  .map(({ item }) => item);
