e3ff9955a02edff82f4397fd9dc7f0f72783cf81
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-24 16:12:28 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +15,69 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_BY_ALPHABET = 'alpha';
+const SORT_BY_LENGTH = 'length';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+export const App = () => {
+  const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  function getSortBy(products, sortBy) {
+    if (sortBy === SORT_BY_ALPHABET) {
+      return () => setVisibleGoods([...products]
+        .sort((a, b) => a.localeCompare(b)));
+    }
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+    if (sortBy === SORT_BY_LENGTH) {
+      return () => setVisibleGoods([...products]
+        .sort((a, b) => a.length - b.length));
+    }
+
+    return () => setVisibleGoods([...visibleGoods].reverse());
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          onClick={getSortBy(goodsFromServer, SORT_BY_ALPHABET)}
+          type="button"
+          className="button is-info is-light"
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          onClick={getSortBy(goodsFromServer, SORT_BY_LENGTH)}
+          type="button"
+          className="button is-success is-light"
+        >
+          Sort by length
+        </button>
+
+        <button
+          onClick={getSortBy(goodsFromServer)}
+          type="button"
+          className="button is-warning is-light"
+        >
+          Reverse
+        </button>
+
+        <button
+          onClick={() => setVisibleGoods([...goodsFromServer])}
+          type="button"
+          className="button is-danger is-light"
+        >
+          Reset
+        </button>
+      </div>
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
