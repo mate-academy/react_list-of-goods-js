315137f2ae10f3cecc07bf2807307aaa7f3d3853
Sergey <serg9205@gmail.com>
2023-08-29 16:03:38 +0300

    prepared solution for the first code review

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import cn from 'classnames';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +16,107 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_ALPHABETICALLY_VALUE = 'Sort alphabetically';
+const SORT_BY_LENGTH_VALUE = 'Sort by length';
+const RESET_VALUE = 'Reset';
+const REVERSE_VALUE = 'Reverse';
+const REVERSE_DEFAULT_VALUE = false;
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function getGoodsSorted(goods, sortKey, reverse) {
+  const tempGoods = [...goods];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (sortKey !== '') {
+    tempGoods.sort((goodA, goodB) => {
+      switch (sortKey) {
+        case SORT_ALPHABETICALLY_VALUE:
+          return goodA.localeCompare(goodB);
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+        case SORT_BY_LENGTH_VALUE:
+          return goodA.length - goodB.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return (reverse !== REVERSE_DEFAULT_VALUE
+    ? tempGoods.reverse()
+    : tempGoods);
+}
+
+export const App = () => {
+  const [sortSelect, setSortSelect] = useState('');
+  const [reverseSelect, setReverseSelect] = useState(REVERSE_DEFAULT_VALUE);
+
+  const sortedGoods = getGoodsSorted(
+    goodsFromServer, sortSelect, reverseSelect,
+  );
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn('button', 'is-info', {
+            'is-light': sortSelect !== SORT_ALPHABETICALLY_VALUE,
+          })}
+          onClick={() => {
+            setSortSelect(SORT_ALPHABETICALLY_VALUE);
+          }}
+        >
+          {SORT_ALPHABETICALLY_VALUE}
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-success', {
+            'is-light': sortSelect !== SORT_BY_LENGTH_VALUE,
+          })}
+          onClick={() => {
+            setSortSelect(SORT_BY_LENGTH_VALUE);
+          }}
+        >
+          {SORT_BY_LENGTH_VALUE}
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-warning', {
+            'is-light': reverseSelect === REVERSE_DEFAULT_VALUE,
+          })}
+          onClick={() => {
+            setReverseSelect(!reverseSelect);
+          }}
+        >
+          {REVERSE_VALUE}
+        </button>
+
+        {(sortSelect || reverseSelect) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setSortSelect('');
+              setReverseSelect(REVERSE_DEFAULT_VALUE);
+            }}
+          >
+            {RESET_VALUE}
+          </button>
+        )}
+
+      </div>
+
+      <ul>
+        {sortedGoods.map(good => (
+          <li
+            data-cy="Good"
+            key={good}
+          >
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
