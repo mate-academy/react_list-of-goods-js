b9d8b8d296c8ec5752943304ac144253841f2a44
Mykhailo <eglet777@ukr.net>
2023-09-17 22:11:43 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +16,108 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+function getGoods(goods, sortParams) {
+  const goodsCopy = [...goods];
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+  if (sortParams.property) {
+    goodsCopy.sort((good1, good2) => {
+      switch (sortParams.property) {
+        case 'name':
+          return good1.localeCompare(good2);
+        case 'length':
+          return good1.length - good2.length;
+        default:
+          return 0;
+      }
+    });
+  }
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (sortParams.reverse) {
+    goodsCopy.reverse();
+  }
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
-    </div>
+  return goodsCopy;
+}
 
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
+const Good = ({ good }) => (
+  <li data-cy="Good">{good}</li>
 );
+
+const GoodList = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <Good good={good} key={good} />
+    ))}
+  </ul>
+);
+
+export const App = () => {
+  const [sortParams, setSortParams] = useState({
+    property: '',
+    reverse: false,
+  });
+  const preparedGoods = getGoods(goodsFromServer, sortParams);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn(
+            'button is-info',
+            { 'is-light': sortParams.property !== 'name' },
+          )}
+          onClick={() => setSortParams({
+            ...sortParams,
+            property: 'name',
+          })}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={cn(
+            'button is-success',
+            { 'is-light': sortParams.property !== 'length' },
+          )}
+          onClick={() => setSortParams({
+            ...sortParams,
+            property: 'length',
+          })}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={cn(
+            'button is-warning',
+            { 'is-light': !sortParams.reverse },
+          )}
+          onClick={() => setSortParams({
+            ...sortParams,
+            reverse: !sortParams.reverse,
+          })}
+        >
+          Reverse
+        </button>
+
+        {(sortParams.property || sortParams.reverse) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => setSortParams({
+              property: '',
+              reverse: false,
+            })}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <GoodList goods={preparedGoods} />
+    </div>
+  );
+};
