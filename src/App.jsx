1b299ab51bb5bc0c25dd874af6d64642dbf871fe
donizer <bogdan.ua45@gmail.com>
2023-10-05 18:06:10 +0300

    list of goods

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import classNames from 'classnames';
 
-export const goodsFromServer = [
+const goodsFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -14,45 +16,129 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_FIELD_NAME = 'name';
+const SORT_FIELD_LENGTH = 'length';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function getPreparedArray(goods, { sortField, reversed }) {
+  let preparedGoods = [...goods];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (sortField) {
+    preparedGoods = preparedGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SORT_FIELD_NAME:
+          if (good1 > good2) {
+            return 1;
+          }
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+          if (good1 < good2) {
+            return -1;
+          }
+
+          return 0;
+
+        case SORT_FIELD_LENGTH:
+          if (good1.length > good2.length) {
+            return 1;
+          }
+
+          if (good1.length < good2.length) {
+            return -1;
+          }
+
+          return 0;
+
+        default: return 0;
+      }
+    });
+  }
+
+  if (reversed) {
+    preparedGoods = preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
+
+export const App = () => {
+  const [sortField, setSortField] = useState('');
+  const [reversed, setReversed] = useState(false);
+
+  const visibleGoods = getPreparedArray(
+    goodsFromServer,
+    { sortField, reversed },
+  );
+
+  const reset = () => {
+    setSortField('');
+    setReversed(false);
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={classNames(
+            'button is-info',
+            {
+              'is-light': sortField !== SORT_FIELD_NAME,
+            },
+          )}
+          onClick={() => setSortField(SORT_FIELD_NAME)}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={classNames(
+            'button is-success',
+            {
+              'is-light': sortField !== SORT_FIELD_LENGTH,
+            },
+          )}
+          onClick={() => setSortField(SORT_FIELD_LENGTH)}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={classNames(
+            'button is-warning',
+            {
+              'is-light': !reversed,
+            },
+          )}
+          onClick={() => setReversed(current => !current)}
+        >
+          Reverse
+        </button>
+
+        {sortField !== '' || reversed !== false
+          ? (
+            <button
+              type="button"
+              className={classNames(
+                'button is-danger',
+                {
+                  'is-light': true,
+                },
+              )}
+              onClick={reset}
+            >
+              Reset
+            </button>
+          )
+          : ''}
+
+      </div>
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li data-cy="Good" key={good}>{good}</li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
