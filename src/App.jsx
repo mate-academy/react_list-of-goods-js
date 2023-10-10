4976412d3b899638c384780f7f557935ae8044d2
YevhenKharko <yevhenkharko@gmail.com>
2023-10-10 18:43:10 +0300

    works fine but fails one test. also, need to refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable max-len */
+/* eslint-disable react/no-array-index-key */
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +18,71 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const Good = ({ good }) => <li data-cy="Good">{good}</li>;
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+export const App = () => {
+  const [goods, setGoods] = useState([...goodsFromServer]);
+  const [activeSorting, setActiveSorting] = useState('');
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn('button is-info', {
+            'is-light': !activeSorting.includes('name'),
+          })}
+          onClick={() => {
+            setGoods([...goods].sort((a, b) => a.localeCompare(b)));
+            setActiveSorting([...activeSorting, 'name']);
+          }}
+        >
+          Sort alphabetically
+        </button>
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+        <button
+          type="button"
+          className={cn('button is-success', {
+            'is-light': !activeSorting.includes('length'),
+          })}
+          onClick={() => {
+            setGoods([...goods].sort((a, b) => a.length - b.length));
+            setActiveSorting([...activeSorting, 'length']);
+          }}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={cn('button is-warning', {
+            'is-light': !activeSorting.includes('reverse'),
+          })}
+          onClick={() => {
+            setGoods(goods.reverse());
+            setActiveSorting([...activeSorting, 'reverse']);
+          }}
+        >
+          Reverse
+        </button>
+
+        {activeSorting && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setGoods(goodsFromServer);
+              setActiveSorting('');
+            }}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goods.map(good => <Good good={good} />)}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
