ff27e44f6c3af69079063603753b4ba6b7c7716c
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2023-10-11 08:52:14 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import {useState} from 'react';
+import cn from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,45 +17,120 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_BY_LENGTH = 'length';
+const SORT_BY_ALPHABET = 'alphabet';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function getSortedGoods(goods, sortParameter, reverseParameter) {
+  const copyGoods = [...goods];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (reverseParameter === 1) {
+    copyGoods.reverse();
+  }
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+  if (sortParameter) {
+    copyGoods.sort((good1, good2) => {
+      switch (sortParameter) {
+        case SORT_BY_LENGTH:
+          if (reverseParameter) {
+            return good2.length - good1.length;
+          }
+
+          return good1.length - good2.length;
+
+        case SORT_BY_ALPHABET:
+          if (reverseParameter) {
+            return good2.localeCompare(good1);
+          }
+
+          return good1.localeCompare(good2);
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return copyGoods;
+}
+
+export const App = () => {
+  const [reverseParameter, setReverseParameter] = useState('');
+  const [sortParameter, setSortParameter] = useState('');
+
+  const visibleGoods
+    = getSortedGoods(goodsFromServer, sortParameter, reverseParameter);
+
+  const reset = () => {
+    setSortParameter('');
+    setReverseParameter('');
+  };
+
+  const reverse = () => {
+    if (reverseParameter) {
+      setReverseParameter('');
+    }
+
+    if (!reverseParameter) {
+      setReverseParameter(1);
+    }
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          onClick={() => setSortParameter('alphabet')}
+          type="button"
+          className={cn('button is-info', {
+            'is-light': sortParameter !== SORT_BY_ALPHABET,
+          })}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          onClick={() => setSortParameter('length')}
+          type="button"
+          className={cn('button is-success', {
+            'is-light': sortParameter !== SORT_BY_LENGTH,
+          })}
+        >
+          Sort by length
+        </button>
+
+        <button
+          onClick={reverse}
+          type="button"
+          className={cn('button is-warning', {
+            'is-light': reverseParameter === '',
+          })}
+        >
+          Reverse
+        </button>
+
+        {(sortParameter || reverseParameter) && (
+          <>
+            <button
+              onClick={reset}
+              type="button"
+              className="button is-danger is-light"
+            >
+              Reset
+            </button>
+          </>
+        )}
+      </div>
+
+      <ul>
+
+        {visibleGoods.map(good => (
+          <li
+            data-cy="Good"
+          >
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
