86f756b337896983fee74fef18b63722f2a49005
Yaroslav <slavik2607.lviv@gmail.com>
2023-10-13 14:31:07 +0300

    Try to fix linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'; // eslint-disable-line
 import cn from 'classnames';
 
 import 'bulma/css/bulma.css';
