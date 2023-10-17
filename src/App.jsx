8ad280fc0feadaaf301422447af9b882b1c29d62
Andrii Solovei <solovey98.info@gmail.com>
2023-10-17 16:57:45 +0300

    renamed the variable and removed the extra state

--- src/App.jsx
+++ src/App.jsx
@@ -18,35 +18,38 @@ export const goodsFromServer = [
 
 const SORT_BY_ALPHABETICALLY = 'alphabet';
 const SORT_BY_LENGTH = 'length';
-const SORT_BY_REVERSE = 'reverse';
+const SORT_BY_REVERSE = 'isReversed';
 
-function getPreparedGood(goods, sortField, revers) {
+function getPreparedGood(goods, sortField, isReversed) {
   const visibleGoods = [...goods];
 
   if (sortField) {
     switch (sortField) {
       case SORT_BY_ALPHABETICALLY:
-        if (revers) {
+        if (isReversed) {
           visibleGoods.sort((good1, good2) => good2.localeCompare(good1));
         } else {
           visibleGoods.sort((good1, good2) => good1.localeCompare(good2));
         }
+
         break;
       case SORT_BY_LENGTH:
-        if (revers) {
+        if (isReversed) {
           visibleGoods.sort((good1, good2) => (
-            good2.length - good1.length))
+            good2.length - good1.length));
         } else {
           visibleGoods.sort((good1, good2) => (
-            good1.length - good2.length))
+            good1.length - good2.length));
         }
-        break;
-      case SORT_BY_REVERSE:
-        visibleGoods.reverse();
+
         break;
       default:
         break;
     }
+  }
+
+  if (sortField === SORT_BY_REVERSE) {
+    visibleGoods.reverse();
   }
 
   return visibleGoods;
@@ -55,41 +58,33 @@ function getPreparedGood(goods, sortField, revers) {
 export const App = () => {
   const [sortField, setSortField] = useState(null);
   const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
-  const [reverse, setReverse] = useState(false);
-  const [isReset, setIsReset] = useState(false);
-
-  const copyVisibleGoods = [...visibleGoods];
+  const [isReversed, setIsReversed] = useState(false);
 
   const sortByAlphabeticaly = () => {
     setSortField(SORT_BY_ALPHABETICALLY);
     setVisibleGoods(
-      getPreparedGood(visibleGoods, SORT_BY_ALPHABETICALLY, reverse),
+      getPreparedGood(visibleGoods, SORT_BY_ALPHABETICALLY, isReversed),
     );
-    setIsReset(true);
   };
 
   const sortByLength = () => {
     setSortField(SORT_BY_LENGTH);
     setVisibleGoods(
-      getPreparedGood(visibleGoods, SORT_BY_LENGTH, reverse),
+      getPreparedGood(visibleGoods, SORT_BY_LENGTH, isReversed),
     );
-    setIsReset(true);
   };
 
   const sortByReverse = () => {
-    setReverse(!reverse);
-    setVisibleGoods(getPreparedGood(visibleGoods, SORT_BY_REVERSE, !reverse));
-    setIsReset(
-      JSON.stringify(copyVisibleGoods.reverse())
-      !== JSON.stringify(goodsFromServer),
+    setIsReversed(!isReversed);
+    setVisibleGoods(
+      getPreparedGood(visibleGoods, SORT_BY_REVERSE, !isReversed)
     );
   };
 
   const resetGoods = () => {
     setSortField('');
     setVisibleGoods(goodsFromServer);
-    setReverse(false);
-    setIsReset(false);
+    setIsReversed(false);
   };
 
   return (
@@ -118,24 +113,25 @@ export const App = () => {
         <button
           type="button"
           className={cn('button is-warning', {
-            'is-light': !reverse,
+            'is-light': !isReversed,
           })}
           onClick={sortByReverse}
         >
           Reverse
         </button>
 
-        {isReset && (
-          <button
-            type="button"
-            className={cn('button is-danger', {
-              'is-light': sortField !== '',
-            })}
-            onClick={resetGoods}
-          >
-            Reset
-          </button>
-        )}
+        {JSON.stringify(visibleGoods)
+          !== JSON.stringify(goodsFromServer) && (
+            <button
+              type="button"
+              className={cn('button is-danger', {
+                'is-light': sortField !== '',
+              })}
+              onClick={resetGoods}
+            >
+              Reset
+            </button>
+          )}
       </div>
 
       <ul>
