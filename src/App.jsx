7c6cdb4c53c5fbbcac4ecce509f605ce570acf66
andrew gidrovich <mr.androvich55555@gmail.com>
2023-10-18 20:21:42 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -20,25 +20,30 @@ export const App = () => {
 
   const [goods, setGoods] = useState(initialGoods);
   const [sortOrder, setSortOrder] = useState('initial');
+  const [isReversed, setIsReversed] = useState(false);
 
   const handleSort = (order) => {
     let sortedGoods = [...goods];
 
-    switch (order) {
-      case 'alphabetical':
-        sortedGoods.sort();
-        break;
-      case 'length':
-        sortedGoods.sort((a, b) => a.length - b.length);
-        break;
-      case 'reverse':
-        sortedGoods.reverse();
-        break;
-      case 'initial':
-        sortedGoods = initialGoods;
-        break;
-      default:
-        break;
+    if (isReversed) {
+      sortedGoods.reverse();
+    } else {
+      switch (order) {
+        case 'alphabetical':
+          sortedGoods.sort((a, b) => a.localeCompare(b));
+          break;
+        case 'length':
+          sortedGoods.sort((a, b) => a.length - b.length);
+          break;
+        case 'reverse':
+          sortedGoods.reverse();
+          break;
+        case 'initial':
+          sortedGoods = initialGoods;
+          break;
+        default:
+          break;
+      }
     }
 
     setGoods(sortedGoods);
@@ -46,6 +51,10 @@ export const App = () => {
   };
 
   const showResetButton = sortOrder !== 'initial';
+
+  const handleReverse = () => {
+    setIsReversed(!isReversed);
+  };
 
   return (
     <div className="section content">
@@ -69,7 +78,10 @@ export const App = () => {
         <button
           type="button"
           className={classNames('button is-warning', { 'is-light': sortOrder !== 'reverse' })}
-          onClick={() => handleSort(sortOrder === 'reverse' ? 'initial' : 'reverse')}
+          onClick={() => {
+            handleSort(sortOrder === 'reverse' ? 'initial' : 'reverse');
+            handleReverse();
+          }}
         >
           Reverse
         </button>
