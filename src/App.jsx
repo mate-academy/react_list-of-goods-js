1a54e4407a90919cd2e493afd319a1ad25c0aa56
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2023-10-31 14:35:33 +0200

    fixed2

--- src/App.jsx
+++ src/App.jsx
@@ -113,7 +113,12 @@ export const App = () => {
 
       <ul>
         {visibleGoods.map(good => (
-          <li data-cy="Good">{good}</li>
+          <li
+            key={good}
+            data-cy="Good"
+          >
+            {good}
+          </li>
         ))}
       </ul>
     </div>
