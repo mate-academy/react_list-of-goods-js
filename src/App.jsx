91f4f396a8486d6bdd336c5393a20f3a8ce356a9
Vladyslav Korobka <vladseipa@gmail.com>
2023-11-03 18:53:56 +0200

    created filter for alphabet, length

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +16,103 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const FILTER_ALPHABETICALLY = 'abc';
+const FILTER_LENGTH = 'length';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+export const App = () => {
+  const [chosenCategory, setChosenCategory] = useState('');
+  const [reverseBtn, setReverseBtn] = useState(false);
+  let sortedGoods = [...goodsFromServer];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  // const handleClickFilter = () => {
+  //   setSortedGoods([...sortedGoods]
+  //     .sort((good1, good2) => good1.localeCompare(good2)));
+  //   setChosenCategory(FILTER_ID);
+  // };
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+  if (chosenCategory) {
+    switch (chosenCategory) {
+      case FILTER_ALPHABETICALLY:
+        sortedGoods = sortedGoods.sort((good1, good2) => {
+          if (reverseBtn) {
+            return good2.localeCompare(good1);
+          }
+
+          return good1.localeCompare(good2);
+        });
+        break;
+
+      case FILTER_LENGTH:
+        sortedGoods = sortedGoods.sort((good1, good2) => {
+          if (reverseBtn) {
+            return good2.length - good1.length;
+          }
+
+          return good1.length - good2.length;
+        });
+        break;
+
+      default:
+        return 0;
+    }
+  }
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={
+            cn('button', 'is-info',
+              { 'is-light': chosenCategory !== FILTER_ALPHABETICALLY })
+          }
+          onClick={() => setChosenCategory(FILTER_ALPHABETICALLY)}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={
+            cn('button', 'is-success',
+              { 'is-light': chosenCategory !== FILTER_LENGTH })
+          }
+          onClick={() => setChosenCategory(FILTER_LENGTH)}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={
+            cn('button', 'is-warning',
+              { 'is-light': !reverseBtn })
+          }
+          onClick={() => setReverseBtn(!reverseBtn)}
+        >
+          Reverse
+        </button>
+
+        {(chosenCategory || reverseBtn)
+          && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setChosenCategory('');
+              setReverseBtn(false);
+            }}
+          >
+            Reset
+          </button>
+          )}
+      </div>
+
+      <ul>
+        {sortedGoods.map((good, index) => (
+          <li data-cy="Good">{good}</li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
