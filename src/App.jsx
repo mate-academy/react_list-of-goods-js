d51496bf08cb9beb9ce0166d8683ba7b05193ca0
VKdrvtsv <valery.volikova1@gmail.com>
2023-11-09 14:13:51 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +15,89 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_FIELD_ALPH = 'alph';
+const SORT_BY_LENGTH = 'length';
+const SORT_REVERSE = 'reverse';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function getSortedGoods(goods, { sortField, direction }) {
+  const sortedGoods = [...goods];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (sortField) {
+    sortedGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SORT_FIELD_ALPH:
+          return good1.localeCompare(good2);
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
-    </div>
+        case SORT_BY_LENGTH:
+          return good1.length - good2.length;
 
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (direction) {
+    sortedGoods.reverse();
+  }
+
+  return sortedGoods;
+}
+
+export const App = () => {
+  const [sortField, setSortField] = useState('');
+  const [direction, setDirection] = useState('');
+  const sortedGoods = getSortedGoods(goodsFromServer, { sortField, direction });
+
+  return (
+    <>
+      <div className="section content">
+        <div className="buttons">
+          <button
+            type="button"
+            className={`button is-info ${sortField !== SORT_FIELD_ALPH && 'is-light'}`}
+            onClick={() => setSortField(SORT_FIELD_ALPH)}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            className={`button is-success ${sortField !== SORT_BY_LENGTH && 'is-light'}`}
+            onClick={() => setSortField(SORT_BY_LENGTH)}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            className={`button is-warning ${direction !== SORT_REVERSE && 'is-light'}`}
+            onClick={() => setDirection(!direction ? SORT_REVERSE : '')}
+          >
+            Reverse
+          </button>
+
+          {(sortField !== '' || direction !== '')
+            && (
+            <button
+              type="button"
+              className="button is-danger is-light"
+              onClick={() => {
+                setSortField('');
+                setDirection('');
+              }}
+            >
+              Reset
+            </button>
+            )}
+        </div>
+
+        <ul>
+          {sortedGoods.map(good => (
+            <li key={good} data-cy="Good">{good}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
