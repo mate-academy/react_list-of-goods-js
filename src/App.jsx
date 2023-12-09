0cfe46777772f4f70e90811f287590b4c14d12c2
boytrend0108 <boytrend@gmail.com>
2023-12-09 17:43:40 +0200

    complete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import 'bulma/css/bulma.css';
+import cn from 'classnames';
 import './App.scss';
+
+import { useState } from 'react';
+import { ProductList } from './components/ProductList';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +18,85 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_BY_ALPHABET = 'alphabet';
+const SORT_BY_LENGTH = 'length';
+const SORT_BY_REVERSE = 'reverse';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+const getPreperedData = (goods, sortedBy) => {
+  const preperedGoods = [...goodsFromServer];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  if (sortedBy === SORT_BY_REVERSE) {
+    return preperedGoods.reverse();
+  }
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+  if (sortedBy === '') {
+    return goodsFromServer;
+  }
+
+  const sortedGoods = preperedGoods.sort((good1, good2) => {
+    switch (sortedBy) {
+      case SORT_BY_ALPHABET:
+        return good1.localeCompare(good2);
+
+      case SORT_BY_LENGTH:
+        return good1.length - good2.length;
+
+      default:
+        return 0;
+    }
+  });
+
+  return sortedGoods;
+};
+
+export const App = () => {
+  const [sortedBy, setSortedBy] = useState('');
+  const goods = getPreperedData(goodsFromServer, sortedBy);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${sortedBy !== SORT_BY_ALPHABET && 'is-light'}`}
+          onClick={() => setSortedBy(SORT_BY_ALPHABET)}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-success', {
+            'is-light': sortedBy !== SORT_BY_LENGTH,
+          })}
+          onClick={() => setSortedBy(SORT_BY_LENGTH)}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-warning', {
+            'is-light': sortedBy !== SORT_BY_REVERSE,
+          })}
+          onClick={() => setSortedBy(SORT_BY_REVERSE)}
+        >
+          Reverse
+        </button>
+
+        {sortedBy !== '' && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => setSortedBy('')}
+          >
+            Reset
+          </button>
+        )}
+
+      </div>
+
+      <ProductList goods={goods} />
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,3 @@
+export const ProductItem = ({ item }) => (
+  <li>{item}</li>
+);
