4805bebf27c85803dbaab5a3487fbae8663938b1
ArtyWhit <julwer1k@gmail.com>
2023-12-13 16:22:18 +0200

    update class add for reverse button

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,7 @@ export const App = () => {
           className={
             cn(
               'button is-warning',
-              { 'is-light': isReversed },
+              { 'is-light': !isReversed },
             )
           }
           onClick={() => {
