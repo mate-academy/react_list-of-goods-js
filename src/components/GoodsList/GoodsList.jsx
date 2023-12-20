23511a30e583f1af77a499cd07b61bc501e8809a
Dmytro Kretov <dmytro.kretov@gmail.com>
2023-12-20 15:09:53 +0200

    key prop added

--- src/components/GoodsList/GoodsList.jsx
+++ src/components/GoodsList/GoodsList.jsx
@@ -3,7 +3,7 @@ import { GoodsCard } from '../GoodsCard/GoodsCard';
 export const GoodsList = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <GoodsCard good={good} />
+      <GoodsCard key={good} good={good} />
     ))}
   </ul>
 );
