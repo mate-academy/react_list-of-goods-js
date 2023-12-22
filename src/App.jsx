51f46db96143272db8bb55edb07852d88c64a08c
Maksym Kostetskyi <mkost@gmail.com>
2023-12-22 13:57:04 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bulma/css/bulma.css';
+import classNames from 'classnames';
 import './App.scss';
 import { useState } from 'react';
 import { GoodList } from './Components/GoodList/GoodList';
@@ -21,41 +22,20 @@ export const goodsFromServer = [
 
 function getPreparedGoods(goods, { sortField, isReversed }) {
   const preparedGoods = [...goods];
-  const preparedForReverse = [...preparedGoods];
-  const reversedGoods = preparedForReverse.reverse();
 
-  if (!isReversed) {
-    preparedGoods.sort((good1, good2) => {
-      switch (sortField) {
-        case SORT_FIELD_NAME:
-          return good1.localeCompare(good2);
-
-        case SORT_FIELD_LENGTH:
-          return good1.length - good2.length;
-
-        default:
-          return 0;
-      }
-    });
-  }
+  preparedGoods.sort((good1, good2) => {
+    switch (sortField) {
+      case SORT_FIELD_NAME:
+        return good1.localeCompare(good2);
+      case SORT_FIELD_LENGTH:
+        return good1.length - good2.length;
+      default:
+        return 0;
+    }
+  });
 
   if (isReversed) {
-    reversedGoods.sort((good1, good2) => {
-      switch (sortField) {
-        case SORT_FIELD_NAME:
-          return good2.localeCompare(good1);
-
-        case SORT_FIELD_LENGTH:
-          return good2.length - good1.length;
-
-        default:
-          return 0;
-      }
-    });
-  }
-
-  if (isReversed) {
-    return reversedGoods;
+    return preparedGoods.reverse();
   }
 
   return preparedGoods;
@@ -64,77 +44,42 @@ function getPreparedGoods(goods, { sortField, isReversed }) {
 export const App = () => {
   const [sortField, setSortField] = useState('');
   const [isReversed, setIsReversed] = useState(false);
+  const prevIsReversed = !isReversed;
   const visibleGoods
   = getPreparedGoods(goodsFromServer, { sortField, isReversed });
 
   return (
     <div className="section content">
       <div className="buttons">
-        {sortField === SORT_FIELD_NAME
-          ? (
-            <button
-              type="button"
-              id="name"
-              className="button is-info"
-              onClick={() => setSortField(SORT_FIELD_NAME)}
-            >
-              Sort alphabetically
-            </button>
-          )
-          : (
-            <button
-              type="button"
-              id="name"
-              className="button is-info is-light"
-              onClick={() => setSortField(SORT_FIELD_NAME)}
-            >
-              Sort alphabetically
-            </button>
-          )}
+        <button
+          type="button"
+          id="name"
+          className={classNames('button is-info',
+            { 'is-light': !(sortField === SORT_FIELD_NAME) })}
+          onClick={() => setSortField(SORT_FIELD_NAME)}
+        >
+          Sort alphabetically
+        </button>
 
-        {sortField === SORT_FIELD_LENGTH
-          ? (
-            <button
-              type="button"
-              id="name"
-              className="button is-success"
-              onClick={() => setSortField(SORT_FIELD_LENGTH)}
-            >
-              Sort by length
-            </button>
-          )
-          : (
-            <button
-              type="button"
-              id="name"
-              className="button is-success is-light"
-              onClick={() => setSortField(SORT_FIELD_LENGTH)}
-            >
-              Sort by length
-            </button>
-          )}
+        <button
+          type="button"
+          id="length"
+          className={classNames('button is-success',
+            { 'is-light': !(sortField === SORT_FIELD_LENGTH) })}
+          onClick={() => setSortField(SORT_FIELD_LENGTH)}
+        >
+          Sort by length
+        </button>
 
-        {isReversed
-          ? (
-            <button
-              type="button"
-              id="reverse"
-              className="button is-warning"
-              onClick={() => setIsReversed(!isReversed)}
-            >
-              Reverse
-            </button>
-          )
-          : (
-            <button
-              type="button"
-              id="reverse"
-              className="button is-warning is-light"
-              onClick={() => setIsReversed(!isReversed)}
-            >
-              Reverse
-            </button>
-          )}
+        <button
+          type="button"
+          id="reverse"
+          className={classNames('button is-warning',
+            { 'is-light': !isReversed })}
+          onClick={() => setIsReversed(prevIsReversed)}
+        >
+          Reverse
+        </button>
 
         {(sortField !== '' || isReversed)
         && (
