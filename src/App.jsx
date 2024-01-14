d23bf8ada0d845c47b6c875cb9c6dc1a722a2dee
Oki <oksana.nastenko89@gmail.com>
2024-01-14 16:09:07 +0100

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,130 @@
-import 'bulma/css/bulma.css';
-import './App.scss';
+import "bulma/css/bulma.css";
+import "./App.scss";
+import { useState } from "react";
 
 export const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
+  "Dumplings",
+  "Carrot",
+  "Eggs",
+  "Ice cream",
+  "Apple",
+  "Bread",
+  "Fish",
+  "Honey",
+  "Jam",
+  "Garlic",
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [status, setStatus] = useState({
+    alphabetical: false,
+    bylength: false,
+    reversed: false,
+  });
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+  const isModified = status.alphabetical || status.bylength || status.reversed;
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  function sortAlphabetically() {
+    setGoods((prevState) => {
+      if (!status.reversed) {
+        return [...prevState].sort();
+      }
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+      return [...prevState].sort((a, b) => b.localeCompare(a));
+    });
+
+    setStatus((prevState) => ({
+      ...prevState,
+      alphabetical: true,
+      bylength: false,
+    }));
+  }
+
+  function sortByLength() {
+    setGoods((prevState) => {
+      if (!status.reversed) {
+        return [...prevState].sort((a, b) => a.length - b.length);
+      }
+
+      return [...prevState].sort((a, b) => b.length - a.length);
+    });
+
+    setStatus((prevState) => ({
+      ...prevState,
+      bylength: true,
+      alphabetical: false,
+    }));
+  }
+
+  function reverseGoods() {
+    setGoods((prevState) => [...prevState].reverse());
+    setStatus((prevState) => ({
+      ...prevState,
+      reversed: !prevState.reversed,
+    }));
+  }
+
+  function resetGoods() {
+    setGoods(goodsFromServer);
+    setStatus({
+      alphabetical: false,
+      bylength: false,
+      reversed: false,
+    });
+  }
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={
+            status.alphabetical ? "button is-info" : "button is-info is-light"
+          }
+          onClick={sortAlphabetically}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={
+            status.bylength ? "button is-success" : "button is-success is-light"
+          }
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={
+            status.reversed ? "button is-warning" : "button is-warning is-light"
+          }
+          onClick={reverseGoods}
+        >
+          Reverse
+        </button>
+
+        {isModified && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={resetGoods}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goods.map((good) => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
