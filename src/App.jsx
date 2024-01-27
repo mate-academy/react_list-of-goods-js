33ea8f373d98024f705982cf7983322475dced71
Eater228 <543145yura@gmail.com>
2024-01-27 14:47:07 +0200

    Add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,45 +16,99 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-      >
-        Sort alphabetically
-      </button>
+const SORT_FIELD_ALPHABET = 'alphabet';
+const SORT_FIELD_LENGTH = 'length';
 
-      <button
-        type="button"
-        className="button is-success is-light"
-      >
-        Sort by length
-      </button>
+function getPrepare(arr, sortField, reverseField) {
+  const prepareArr = [...arr];
 
-      <button
-        type="button"
-        className="button is-warning is-light"
-      >
-        Reverse
-      </button>
+  switch (sortField) {
+    case SORT_FIELD_ALPHABET:
+      prepareArr.sort();
+      break;
+    case SORT_FIELD_LENGTH:
+      prepareArr.sort((a, b) => a.length - b.length);
+      break;
 
-      <button
-        type="button"
-        className="button is-danger is-light"
-      >
-        Reset
-      </button>
+    default:
+      break;
+  }
+
+  if (reverseField) {
+    prepareArr.reverse();
+  }
+
+  return prepareArr;
+}
+
+export const App = () => {
+  const [sortField, setSortField] = useState('');
+  const [reverseField, setReverseField] = useState(false);
+  const visebl = getPrepare(goodsFromServer, sortField, reverseField);
+
+  const reset = () => {
+    setSortField('');
+    setReverseField(false);
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={
+            cn('button is-info',
+              { 'is-light': sortField !== SORT_FIELD_ALPHABET })
+          }
+          onClick={() => setSortField(SORT_FIELD_ALPHABET)}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={
+            cn('button is-success',
+              { 'is-light': sortField !== SORT_FIELD_LENGTH })
+          }
+          onClick={() => setSortField(SORT_FIELD_LENGTH)}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={
+            cn('button is-warning',
+              { 'is-light': reverseField === false })
+          }
+          onClick={() => setReverseField(current => !current)}
+        >
+          Reverse
+        </button>
+
+        {(sortField || reverseField) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
+        )}
+
+      </div>
+      <ul>
+        {visebl.map(element => (
+          <li
+            data-cy="Good"
+            key={element}
+          >
+            {element}
+          </li>
+        ))}
+      </ul>
+
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
