5866f4644eb5281c76b44cd6672ed04f28b84f73
Andrew Rich <andy.rich3a@gmail.com>
2024-02-28 11:02:28 +0200

    add task fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,7 @@ const SORT_FIELD_ALPHABET = 'alphabetically';
 const SORT_FIELD_LENGTH = 'length';
 
 function getPreparedGoods(goods, { sortfield, query, reverse }) {
-  let preparedGoods = [...goods];
-
-  if (query) {
-    preparedGoods = preparedGoods
-      .filter(good => good.name
-        .toLowerCase()
-        .includes(query.toLowerCase()));
-  }
+  const preparedGoods = [...goods];
 
   if (sortfield) {
     preparedGoods.sort((good1, good2) => {
@@ -60,13 +53,12 @@ function getPreparedGoods(goods, { sortfield, query, reverse }) {
           break;
 
         default:
-          return 0;
+          return comparisonResult;
       }
 
       return comparisonResult;
     });
   } else if (reverse) {
-    // Reverse the goods if there is no sorting but the reverse flag is true
     preparedGoods.reverse();
   }
 
@@ -77,58 +69,18 @@ export const App = () => {
   const [sortfield, setSortField] = useState('');
   const [reverse, setReverse] = useState(false);
   const [query] = useState('');
-  const [originalGoods, setOriginalGoods] = useState([...goodsFromServer]);
 
   const handleSort = (field) => {
     const newReverse = field === sortfield ? !reverse : reverse; // Keep the current reverse state if the same field is clicked
 
     setSortField(field);
-
-    const sortedGoods = [...originalGoods];
-
-    // Sort the goods
-    sortedGoods.sort((good1, good2) => {
-      switch (field) {
-        case SORT_FIELD_ALPHABET:
-          return newReverse
-            ? good2.name.localeCompare(good1.name)
-            : good1.name.localeCompare(good2.name);
-        case SORT_FIELD_LENGTH: {
-          const lengthDiff = newReverse
-            ? good2.name.length - good1.name.length
-            : good1.name.length - good2.name.length;
-          let result;
-
-          if (lengthDiff !== 0) {
-            result = lengthDiff;
-          } else {
-            result = newReverse
-              ? good2.name.localeCompare(good1.name)
-              : good1.name.localeCompare(good2.name);
-          }
-
-          return result;
-        }
-
-        default:
-          return 0;
-      }
-    });
-
     setReverse(newReverse);
-    setOriginalGoods(sortedGoods);
   };
 
   const handleReverse = () => {
-    // If it's the initial reverse, reverse the original goods
-    if ((!sortfield && !reverse)) {
-      const reversedGoods = [...originalGoods].reverse();
-
-      setOriginalGoods(reversedGoods);
+    if (!sortfield && !reverse) {
       setReverse(true);
-      setSortField('');
     } else {
-      // If it's a reversal after sorting, reverse the sorted goods
       setReverse(!reverse);
     }
   };
@@ -136,46 +88,39 @@ export const App = () => {
   const handleReset = () => {
     setSortField('');
     setReverse(false);
-    setOriginalGoods([...goodsFromServer]);
   };
 
-  const visibleGoods
-    = getPreparedGoods(goodsFromServer, { sortfield, query, reverse });
-  const sortedGoods = [...visibleGoods];
+  const visibleGoods = getPreparedGoods(goodsFromServer, {
+    sortfield,
+    query,
+    reverse,
+  });
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className={cn(
-            'button is-info',
-            {
-              'is-light':
-                sortfield !== SORT_FIELD_ALPHABET
-                || (reverse && sortfield !== SORT_FIELD_ALPHABET),
-            },
-          )}
+          className={cn('button is-info', {
+            'is-light':
+              sortfield !== SORT_FIELD_ALPHABET
+              || (reverse && sortfield !== SORT_FIELD_ALPHABET),
+          })}
           onClick={() => handleSort(SORT_FIELD_ALPHABET)}
         >
           Sort alphabetically
         </button>
-
         <button
           type="button"
-          className={cn(
-            'button is-success',
-            {
-              'is-light':
-                sortfield !== SORT_FIELD_LENGTH
-                || (reverse && sortfield !== SORT_FIELD_LENGTH),
-            },
-          )}
+          className={cn('button is-success', {
+            'is-light':
+              sortfield !== SORT_FIELD_LENGTH
+              || (reverse && sortfield !== SORT_FIELD_LENGTH),
+          })}
           onClick={() => handleSort(SORT_FIELD_LENGTH)}
         >
           Sort by length
         </button>
-
         <button
           type="button"
           className={cn('button is-warning', { 'is-light': !reverse })}
@@ -183,8 +128,7 @@ export const App = () => {
         >
           Reverse
         </button>
-
-        {(sortfield || reverse || query) && (
+        {(sortfield || reverse) && (
           <button
             type="button"
             className={cn('button is-danger', 'is-light')}
@@ -194,10 +138,11 @@ export const App = () => {
           </button>
         )}
       </div>
-
       <ul>
-        {sortedGoods.map(good => (
-          <li key={good.id} data-cy="Good">{good.name}</li>
+        {visibleGoods.map(good => (
+          <li key={good.id} data-cy="Good">
+            {good.name}
+          </li>
         ))}
       </ul>
     </div>
