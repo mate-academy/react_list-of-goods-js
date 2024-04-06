b0e077e1cd3d43a955d87b1426eac7dabf8a7003
Illia <ilyaivasyk1@gmail.com>
2024-04-06 13:10:45 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +15,106 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+function sortByAlphabetically(copyAlphabeticallyArray) {
+  const sortAlphabeticallyArray = [...copyAlphabeticallyArray];
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  return sortAlphabeticallyArray.sort((good1, good2) =>
+    good1.localeCompare(good2),
+  );
+}
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+function sortByLength(copyLengthArray) {
+  const sortLengthArray = [...copyLengthArray];
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  return sortLengthArray.sort((good1, good2) => good1.length - good2.length);
+}
+
+function reverseCopyArray(copyReverseArray) {
+  const arrayReverse = [...copyReverseArray];
+
+  return arrayReverse.reverse();
+}
+
+export function App() {
+  const [goodValue, setGoodValue] = useState([...goodsFromServer]);
+  const [flag, setFlag] = useState(false);
+  const [activeMethod, setActiveMethod] = useState(null);
+
+  const sortAlphabetically = () => {
+    const sortedGoods = sortByAlphabetically(goodValue);
+
+    setGoodValue(sortedGoods);
+    setActiveMethod('alphabetically');
+    setFlag(true);
+  };
+
+  const sortLength = () => {
+    const sortedGoods = sortByLength(goodValue);
+
+    setGoodValue(sortedGoods);
+    setActiveMethod('length');
+    setFlag(true);
+  };
+
+  const reverseArr = () => {
+    const reverseArray = reverseCopyArray(goodValue);
+
+    setGoodValue(reverseArray);
+    setActiveMethod('reverse');
+    setFlag(true);
+  };
+
+  const resetOrder = () => {
+    setGoodValue([...goodsFromServer]);
+    setActiveMethod(null);
+    setFlag(false);
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${activeMethod === 'alphabetical' ? '' : 'is-light'}`}
+          onClick={sortAlphabetically}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={`button is-success ${activeMethod === 'length' ? '' : 'is-light'}`}
+          onClick={sortLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={`button is-warning ${activeMethod === 'reverse' ? '' : 'is-light'}`}
+          onClick={reverseArr}
+        >
+          Reverse
+        </button>
+
+        {flag && (
+          <button
+            type="button"
+            className={`button is-danger ${activeMethod === null ? '' : 'is-light'}`}
+            onClick={resetOrder}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goodValue.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+}
