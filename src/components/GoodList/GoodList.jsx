84498a55b4bf07fecb73dad63e8493f7f373f713
Vasyl Chukhrai <wasik1995@gmail.com>
2024-04-09 10:15:15 +0300

    add task solution

--- src/components/GoodList/GoodList.jsx
+++ src/components/GoodList/GoodList.jsx
@@ -2,8 +2,8 @@ import { GoodCard } from '../GoodCard';
 
 export const GoodList = ({ goods }) => (
   <ul>
-    {goods.map((good) => (
-      <GoodCard good={good} />
+    {goods.map(good => (
+      <GoodCard good={good} key={good}/>
     ))}
   </ul>
 );
