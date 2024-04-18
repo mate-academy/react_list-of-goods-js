0d848f9ab4d2055b87bfbf928176de3d39c9c2fc
Valentyn Sobol <valiksobol14@gmail.com>
2024-04-18 18:19:19 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
+import classNames from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,91 +16,85 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-const SORT_BY_ALPHABET = {
-  id: 1,
-  title: 'Sort alphabetically',
-  class: 'is-info',
-};
-const SORT_BY_LENGTH = {
-  id: 2,
-  title: 'Sort by length',
-  class: 'is-success',
-};
-const REVERSE = {
-  id: 3,
-  title: 'Reverse',
-  class: 'is-warning',
-};
-const RESET = {
-  id: 4,
-  title: 'Reset',
-  class: 'is-danger',
+const SORT_BY_ALPHABET = 'alphabet';
+const SORT_BY_LENGTH = 'length';
+const prepareGoods = (goods, { sortedBtn, isReverse }) => {
+  const newGoods = [...goods];
+
+  if (sortedBtn) {
+    newGoods.sort((good1, good2) => {
+      switch (sortedBtn) {
+        case SORT_BY_ALPHABET:
+          return good1.localeCompare(good2);
+        case SORT_BY_LENGTH:
+          return good1.length - good2.length;
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (isReverse) {
+    newGoods.reverse();
+  }
+
+  return newGoods;
 };
 
 export const App = () => {
-  const [sortBtn, setSortBtn] = useState('');
-  const [sortedGoods, setSortedGoods] = useState([...goodsFromServer]);
-  const [onReverse, setOnReverse] = useState('none');
-  const sortOnClick = sort => {
-    let newSortedGoods = [...sortedGoods];
-
-    switch (sort) {
-      case SORT_BY_ALPHABET:
-        newSortedGoods = [...goodsFromServer].sort((a, b) =>
-          a.localeCompare(b),
-        );
-        break;
-      case SORT_BY_LENGTH:
-        newSortedGoods = [...goodsFromServer].sort(
-          (a, b) => a.length - b.length,
-        );
-        break;
-      case REVERSE:
-        newSortedGoods.reverse();
-        break;
-      case RESET:
-        newSortedGoods = [...goodsFromServer];
-        break;
-      default:
-        break;
-    }
-
-    const sortedGoodsString = newSortedGoods.join('');
-    const goodsFromServerString = goodsFromServer.join('');
-
-    if (sortedGoodsString !== goodsFromServerString) {
-      setOnReverse('block');
-    } else {
-      setOnReverse('none');
-    }
-
-    setSortedGoods(newSortedGoods);
-  };
+  const [isReverse, setIsReverse] = useState(false);
+  const [sortedBtn, setSortedBtn] = useState('');
+  const finalGoods = prepareGoods(goodsFromServer, { sortedBtn, isReverse });
 
   return (
     <div className="section content">
       <div className="buttons">
-        {[SORT_BY_ALPHABET, SORT_BY_LENGTH, REVERSE, RESET].map(type => (
+        <button
+          type="button"
+          onClick={() => setSortedBtn(SORT_BY_ALPHABET)}
+          className={classNames('button is-info', {
+            'is-light': sortedBtn !== SORT_BY_ALPHABET,
+          })}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          onClick={() => setSortedBtn(SORT_BY_LENGTH)}
+          className={classNames('button is-success', {
+            'is-light': sortedBtn !== SORT_BY_LENGTH,
+          })}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          onClick={() => setIsReverse(!isReverse)}
+          className={classNames('button is-warning', {
+            'is-light': !isReverse,
+          })}
+        >
+          Reverse
+        </button>
+
+        {(sortedBtn || isReverse) && (
           <button
-            key={type.id}
-            type="button"
-            className={
-              sortBtn === type
-                ? `button ${type.class}`
-                : `button ${type.class} is-light`
-            }
             onClick={() => {
-              setSortBtn(type);
-              sortOnClick(type);
+              setSortedBtn('');
+              setIsReverse(false);
             }}
-            style={type === RESET ? { display: onReverse } : {}}
+            type="button"
+            className="button is-danger is-light"
           >
-            {type.title}
+            Reset
           </button>
-        ))}
+        )}
       </div>
+
       <ul>
-        {sortedGoods.map(good => (
+        {finalGoods.map(good => (
           <li key={good} data-cy="Good">
             {good}
           </li>
