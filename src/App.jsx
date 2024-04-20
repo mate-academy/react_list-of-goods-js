c0c966ff2039e1e8fbfbab4ccf9fd3996e4ef372
Denys Lysiak <lisyak99.dl@gmail.com>
2024-04-20 14:17:17 +0300

    add suggested changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
+import cn from 'classnames';
 import { List } from './components/listOfGoods';
 
 export const goodsFromServer = [
@@ -20,56 +21,48 @@ const SORT_FIELD_LENGTH = 'length';
 const SORT_FIELD_ALPHABETICALLY = 'alphabetically';
 const SORT_FIELD_REVERSE = 'reverse';
 
+function getPreparedGoods(goods, sortField, reverseField) {
+  const preparedGoods = [...goods];
+
+  if (sortField) {
+    preparedGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SORT_FIELD_LENGTH:
+          return good1.length - good2.length;
+
+        case SORT_FIELD_ALPHABETICALLY:
+          return good1.localeCompare(good2);
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return reverseField === SORT_FIELD_REVERSE
+    ? preparedGoods.reverse()
+    : preparedGoods;
+}
+
 export const App = () => {
-  const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
   const [sortField, setSortField] = useState('');
   const [sortFieldReverse, setSortFieldReverse] = useState('');
 
-  const reset = () => {
-    setVisibleGoods(goodsFromServer);
-    setSortField('');
-    setSortFieldReverse('');
-  };
-
-  const sortByLength = () => {
-    if (sortFieldReverse === SORT_FIELD_REVERSE) {
-      setVisibleGoods(
-        [...visibleGoods].sort((good1, good2) => good2.length - good1.length),
-      );
-    } else {
-      setVisibleGoods(
-        [...visibleGoods].sort((good1, good2) => good1.length - good2.length),
-      );
-    }
-
-    setSortField(SORT_FIELD_LENGTH);
-  };
-
-  const reverseSort = () => {
-    setVisibleGoods([...visibleGoods].reverse());
-    if (sortFieldReverse === '') {
-      setSortFieldReverse(SORT_FIELD_REVERSE);
-    } else {
-      setSortFieldReverse('');
-    }
-  };
-
-  const sortByAlphabet = () => {
-    if (sortFieldReverse === SORT_FIELD_REVERSE) {
-      setVisibleGoods(
-        [...visibleGoods].sort((good1, good2) => good2.localeCompare(good1)),
-      );
-    } else {
-      setVisibleGoods(
-        [...visibleGoods].sort((good1, good2) => good1.localeCompare(good2)),
-      );
-    }
-
-    setSortField(SORT_FIELD_ALPHABETICALLY);
-  };
+  const visibleGoods = getPreparedGoods(
+    goodsFromServer,
+    sortField,
+    sortFieldReverse,
+  );
 
   let buttonReset = (
-    <button type="button" className="button is-danger is-light" onClick={reset}>
+    <button
+      type="button"
+      className="button is-danger is-light"
+      onClick={() => {
+        setSortField('');
+        setSortFieldReverse('');
+      }}
+    >
       Reset
     </button>
   );
@@ -83,24 +76,40 @@ export const App = () => {
       <div className="buttons">
         <button
           type="button"
-          className={`button is-info ${sortField === SORT_FIELD_ALPHABETICALLY ? '' : 'is-light'}`}
-          onClick={sortByAlphabet}
+          className={cn('button is-info', {
+            'is-light': sortField !== SORT_FIELD_ALPHABETICALLY,
+          })}
+          onClick={() => {
+            setSortField(SORT_FIELD_ALPHABETICALLY);
+          }}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className={`button is-success ${sortField === SORT_FIELD_LENGTH ? '' : 'is-light'}`}
-          onClick={sortByLength}
+          className={cn('button is-success', {
+            'is-light': sortField !== SORT_FIELD_LENGTH,
+          })}
+          onClick={() => {
+            setSortField(SORT_FIELD_LENGTH);
+          }}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className={`button is-warning ${sortFieldReverse === SORT_FIELD_REVERSE ? '' : 'is-light'}`}
-          onClick={reverseSort}
+          className={cn('button is-warning', {
+            'is-light': sortFieldReverse !== SORT_FIELD_REVERSE,
+          })}
+          onClick={() => {
+            if (sortFieldReverse === '') {
+              setSortFieldReverse(SORT_FIELD_REVERSE);
+            } else {
+              setSortFieldReverse('');
+            }
+          }}
         >
           Reverse
         </button>
