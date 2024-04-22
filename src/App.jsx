4e81b1e83b27ebb34c32a3f8b366da4415a1f0a9
marchAlbion <marchAlbion@gmail.com>
2024-04-22 14:54:55 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,19 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
-
-export const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
-const SORT_ALPHABET = 'alphabet';
-const SORT_LENGTH = 'length';
-const RESET = 'reset';
-const REVERSE = false;
-
-function filteredGoods(goods, filter, reversed, setFilter) {
-  let result = [...goods];
-
-  if (filter === SORT_ALPHABET) {
-    result.sort((a, b) => a.localeCompare(b));
-  }
-
-  if (filter === SORT_LENGTH) {
-    result.sort((a, b) => a.length - b.length);
-  }
-
-  if (reversed === true) {
-    result = result.reverse();
-  }
-
-  if (filter === RESET) {
-    result = [...goodsFromServer];
-    setFilter('');
-  }
-
-  return result;
-}
+import {
+  goodsFromServer,
+  SORT_TYPE,
+  filteredGoods,
+} from './utils/filteredGoods';
 
 export const App = () => {
   const [filter, setFilter] = useState('');
-  const [reverseToggle, setReverseToggle] = useState(REVERSE);
+  const [reverseToggle, setReverseToggle] = useState(SORT_TYPE.REVERSE);
 
   function reset() {
-    setFilter(RESET);
-    setReverseToggle(REVERSE);
+    setFilter(SORT_TYPE.RESET);
+    setReverseToggle(SORT_TYPE.REVERSE);
   }
 
   const goods = filteredGoods(
@@ -63,16 +28,16 @@ export const App = () => {
       <div className="buttons">
         <button
           type="button"
-          className={`button is-info ${filter !== SORT_ALPHABET && 'is-light'}`}
-          onClick={() => setFilter(SORT_ALPHABET)}
+          className={`button is-info ${filter !== SORT_TYPE.SORT_ALPHABET && 'is-light'}`}
+          onClick={() => setFilter(SORT_TYPE.SORT_ALPHABET)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className={`button is-success ${filter !== SORT_LENGTH && 'is-light'}`}
-          onClick={() => setFilter(SORT_LENGTH)}
+          className={`button is-success ${filter !== SORT_TYPE.SORT_LENGTH && 'is-light'}`}
+          onClick={() => setFilter(SORT_TYPE.SORT_LENGTH)}
         >
           Sort by length
         </button>
@@ -92,9 +57,7 @@ export const App = () => {
           >
             Reset
           </button>
-        ) : (
-          ''
-        )}
+        ) : null}
       </div>
 
       <ul>
