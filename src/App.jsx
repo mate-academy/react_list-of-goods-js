1024c550342ab1b7cad1267945e1bda9a914fcf6
Arthur Fujii <arthurfujii@gmail.com>
2024-05-02 15:25:32 -0300

    key fix

--- src/App.jsx
+++ src/App.jsx
@@ -123,7 +123,7 @@ export const App = () => {
 
       <ul>
         {visibleGoods.map((good, index) => (
-          <li data-cy="Good" key={index}>
+          <li data-cy="Good" key={`${good}+${index + 1}`}>
             {good}
           </li>
         ))}
