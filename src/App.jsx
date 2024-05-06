b1bf52aa11c9024e2c0b51ac338bb412709ee32d
Denys Povh <kirilloffdanza@gmail.com>
2024-05-07 01:54:26 +0300

    solutoin

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +16,103 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+const GoodCard = ({ good }) => <li data-cy="Good">{good}</li>;
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
-
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
-
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
-    </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
+const Goodlist = ({ goods }) => (
+  <ul>
+    {goods.map((good, index) => (
+      <GoodCard good={good} />
+    ))}
+  </ul>
 );
+
+export const App = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+  const [reversed, setReversed] = useState(false);
+  const [sortField, setSortField] = useState('');
+  const [showReset, setShowReset] = useState(false);
+
+  let visibleGoods = goodsFromServer;
+
+  if (selectedColor) {
+    visibleGoods = visibleGoods.filter(good => good.color === selectedColor);
+  }
+
+  if (sortField === 'alphabetically') {
+    visibleGoods = [...visibleGoods].sort((a, b) => a.localeCompare(b));
+  } else if (sortField === 'byLength') {
+    visibleGoods = [...visibleGoods].sort((a, b) => a.length - b.length);
+  }
+
+  if (reversed) {
+    visibleGoods = visibleGoods.reverse();
+  }
+
+  const handleReverseClick = () => {
+    setReversed(!reversed);
+    setShowReset(true);
+  };
+
+  const handleResetClick = () => {
+    setSelectedColor('');
+    setReversed(false);
+    setSortField('');
+    setShowReset(false);
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={classNames('button', 'is-info', {
+            'is-light': sortField !== 'alphabetically',
+          })}
+          onClick={() => {
+            setSortField('alphabetically');
+            setShowReset(true);
+          }}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={classNames('button', 'is-success', {
+            'is-light': sortField !== 'byLength',
+          })}
+          onClick={() => {
+            setSortField('byLength');
+            setShowReset(true);
+          }}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={classNames('button', 'is-warning', {
+            'is-light': !reversed,
+          })}
+          onClick={handleReverseClick}
+        >
+          Reverse
+        </button>
+
+        {showReset && (
+          <button
+            type="button"
+            className={classNames('button', 'is-danger', {
+              'is-light': !'',
+            })}
+            onClick={handleResetClick}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <Goodlist goods={visibleGoods} />
+    </div>
+  );
+};
