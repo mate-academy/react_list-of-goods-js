0ab54932a799555f02e1a3f4365f533da41fe886
Pasha Sich <97337742+Kapidastra@users.noreply.github.com>
2024-05-26 15:39:42 +0300

    Add solution fixed part 2

--- src/App.jsx
+++ src/App.jsx
@@ -21,57 +21,60 @@ const SORT_ALPHABETICALLY = 'sortAlphabetically';
 const REVERSE_LIST = 'reverseList';
 const RESET = 'reset';
 
-function getPreparedGoodsFromServer(goods, buttonId) {
+function getPreparedGoodsFromServer(goods, buttonId, isReversed) {
+  let sortedGoods;
+
   switch (buttonId) {
     case SORT_ALPHABETICALLY:
-      return [...goods].sort((good1, good2) => good1.localeCompare(good2));
+      sortedGoods = [...goods].sort((good1, good2) =>
+        good1.localeCompare(good2),
+      );
+      break;
     case SORT_BY_LENGTH:
-      return [...goods].sort((good1, good2) => good1.length - good2.length);
-    case REVERSE_LIST:
-      return [...goodsFromServer].reverse();
-    case RESET:
-      return [...goodsFromServer];
+      sortedGoods = [...goods].sort(
+        (good1, good2) => good1.length - good2.length,
+      );
+      break;
     default:
-      return goods;
+      sortedGoods = goods;
   }
+
+  if (isReversed) {
+    sortedGoods.reverse();
+  }
+
+  return sortedGoods;
 }
 
 export const App = () => {
-  const [visibleVegetables, setVisibleVegetables] = useState([
-    ...goodsFromServer,
-  ]);
-  const [buttonClassSortAlpha, setButtonClassSortAlpha] =
-    useState('is-info is-light');
-  const [buttonClassSortByLength, setButtonClassSortByLength] = useState(
-    'is-success is-light',
-  );
-  const [buttonClassReverse, setButtonClassReverse] = useState(
-    'is-warning is-light',
-  );
-  const [buttonClassReset, setButtonClassReset] =
-    useState('is-danger is-light');
+  const [sortingType, setSortingType] = useState(null);
+  const [isReversed, setIsReversed] = useState(false);
 
   const buttonClick = buttonId => {
-    const sortedGoods = getPreparedGoodsFromServer(visibleVegetables, buttonId);
-
-    setVisibleVegetables(sortedGoods);
-
-    setButtonClassSortAlpha(
-      buttonId === SORT_ALPHABETICALLY ? 'is-info' : 'is-info is-light',
-    );
-    setButtonClassSortByLength(
-      buttonId === SORT_BY_LENGTH ? 'is-success' : 'is-success is-light',
-    );
-    setButtonClassReverse(
-      buttonId === REVERSE_LIST ? 'is-warning' : 'is-warning is-light',
-    );
-    setButtonClassReset(
-      buttonId === RESET ? 'is-danger' : 'is-danger is-light',
-    );
+    switch (buttonId) {
+      case SORT_ALPHABETICALLY:
+        setSortingType(SORT_ALPHABETICALLY);
+        setIsReversed(false);
+        break;
+      case SORT_BY_LENGTH:
+        setSortingType(SORT_BY_LENGTH);
+        setIsReversed(false);
+        break;
+      case REVERSE_LIST:
+        setIsReversed(!isReversed);
+        break;
+      case RESET:
+        setSortingType(null);
+        setIsReversed(false);
+        break;
+      default:
+        break;
+    }
   };
 
   const resetGoods = () => {
-    setVisibleVegetables([...goodsFromServer]);
+    setSortingType(null);
+    setIsReversed(false);
   };
 
   const isDifferent = () => {
@@ -80,13 +83,24 @@ export const App = () => {
     );
   };
 
+  const visibleVegetables = isReversed
+    ? [...goodsFromServer].reverse()
+    : [...goodsFromServer];
+
+  const preparedGoods = sortingType
+    ? getPreparedGoodsFromServer(visibleVegetables, sortingType, isReversed)
+    : visibleVegetables;
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           onClick={() => buttonClick(SORT_ALPHABETICALLY)}
           type="button"
-          className={cn('button', buttonClassSortAlpha)}
+          className={cn('button', {
+            'is-info': sortingType === SORT_ALPHABETICALLY,
+            'is-info is-light': sortingType !== SORT_ALPHABETICALLY,
+          })}
         >
           Sort alphabetically
         </button>
@@ -94,7 +108,10 @@ export const App = () => {
         <button
           onClick={() => buttonClick(SORT_BY_LENGTH)}
           type="button"
-          className={cn('button', buttonClassSortByLength)}
+          className={cn('button', {
+            'is-success': sortingType === SORT_BY_LENGTH,
+            'is-success is-light': sortingType !== SORT_BY_LENGTH,
+          })}
         >
           Sort by length
         </button>
@@ -102,16 +119,19 @@ export const App = () => {
         <button
           onClick={() => buttonClick(REVERSE_LIST)}
           type="button"
-          className={cn('button', buttonClassReverse)}
+          className={cn('button', {
+            'is-warning': isReversed,
+            'is-warning is-light': !isReversed,
+          })}
         >
           Reverse
         </button>
 
-        {isDifferent() && (
+        {(isDifferent() || isReversed || sortingType) && (
           <button
             onClick={resetGoods}
             type="button"
-            className={cn('button', buttonClassReset)}
+            className={cn('button', 'is-danger')}
           >
             Reset
           </button>
@@ -119,7 +139,7 @@ export const App = () => {
       </div>
 
       <ul>
-        {visibleVegetables.map(vegetable => (
+        {preparedGoods.map(vegetable => (
           <li key={vegetable} data-cy="Good">
             {vegetable}
           </li>
