30fdcaf5c2afce67c2862a4b5db69c5dc90aebb9
Vitalii <shut.vitaliy.ol@gmail.com>
2024-06-16 20:55:10 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
+import React from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -46,64 +48,66 @@ export const App = () => {
   };
 
   return (
-    <div className="section content">
-      <div className="buttons">
-        <button
-          type="button"
-          className={`button is-info ${alphabet ? '' : 'is-light'}`}
-          onClick={() =>
-            setTrackStatus({
-              ...trackStatus,
-              alphabet: !alphabet,
-              isLength: false,
-            })
-          }
-        >
-          Sort alphabetically
-        </button>
-
-        <button
-          type="button"
-          className={`button is-success ${isLength ? '' : 'is-light'}`}
-          onClick={() =>
-            setTrackStatus({
-              ...trackStatus,
-              isLength: !isLength,
-              alphabet: false,
-            })
-          }
-        >
-          Sort by length
-        </button>
-
-        <button
-          type="button"
-          className={`button is-warning ${reversed ? '' : 'is-light'}`}
-          onClick={() =>
-            setTrackStatus({ ...trackStatus, reversed: !reversed })
-          }
-        >
-          Reverse
-        </button>
-
-        {(reversed || isLength || alphabet) && (
+    <>
+      <div className="section content">
+        <div className="buttons">
           <button
             type="button"
-            className="button is-danger is-light"
-            onClick={() => setTrackStatus(defaultState)}
+            className={`button is-info ${cn({ 'is-light': !alphabet })}`}
+            onClick={() =>
+              setTrackStatus({
+                ...trackStatus,
+                alphabet: !alphabet,
+                isLength: false,
+              })
+            }
           >
-            Reset
+            Sort alphabetically
           </button>
-        )}
-      </div>
 
-      <ul>
-        {getGoods().map(good => (
-          <li data-cy="Good" key={good}>
-            {good}
-          </li>
-        ))}
-      </ul>
-    </div>
+          <button
+            type="button"
+            className={`button is-success ${cn({ 'is-light': !isLength })}`}
+            onClick={() =>
+              setTrackStatus({
+                ...trackStatus,
+                isLength: !isLength,
+                alphabet: false,
+              })
+            }
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            className={`button is-warning ${cn({ 'is-light': !reversed })}`}
+            onClick={() =>
+              setTrackStatus({ ...trackStatus, reversed: !reversed })
+            }
+          >
+            Reverse
+          </button>
+
+          {(reversed || isLength || alphabet) && (
+            <button
+              type="button"
+              className="button is-danger is-light"
+              onClick={() => setTrackStatus(defaultState)}
+            >
+              Reset
+            </button>
+          )}
+        </div>
+
+        <ul>
+          {getGoods().map(good => (
+            <li data-cy="Good" key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
