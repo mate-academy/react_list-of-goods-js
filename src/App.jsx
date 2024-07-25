b412cf359195734eb99ac744a496a159737bcd93
Vladislav <njiuyfqqq123@gmail.com>
2024-07-26 00:29:41 +0300

    success

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import 'bulma/css/bulma.css';
-import './App.scss';
 import { useState } from 'react';
 import classNames from 'classnames';
+import 'bulma/css/bulma.css';
+import './App.scss';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -18,35 +18,45 @@ export const goodsFromServer = [
 
 const SORT_FIELD_ALPHABET = 'alphabet';
 const SORT_FIELD_LENGTH = 'length';
-const SORT_FIELD_REVERSE = 'reverse';
 
 export const App = () => {
-  const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
   const [sortField, setSortField] = useState('');
+  const [isReversed, setIsReversed] = useState(false);
+
+  const getVisibleGoods = (goods, valueSortField, valueIsReversed) => {
+    let sortedGoods = [...goods];
+
+    if (valueSortField === SORT_FIELD_ALPHABET) {
+      sortedGoods.sort((good1, good2) => good1.localeCompare(good2));
+    } else if (valueSortField === SORT_FIELD_LENGTH) {
+      sortedGoods.sort((good1, good2) => good1.length - good2.length);
+    }
+
+    if (valueIsReversed) {
+      sortedGoods.reverse();
+    }
+
+    return sortedGoods;
+  };
 
   const sortByAlphabet = () => {
-    setVisibleGoods(
-      [...visibleGoods].sort((good1, good2) => good1.localeCompare(good2)),
-    );
     setSortField(SORT_FIELD_ALPHABET);
   };
 
   const sortByLength = () => {
-    setVisibleGoods(
-      [...visibleGoods].sort((good1, good2) => good1.length - good2.length),
-    );
     setSortField(SORT_FIELD_LENGTH);
   };
 
   const reverse = () => {
-    setVisibleGoods([...visibleGoods].reverse());
-    setSortField(SORT_FIELD_REVERSE);
+    setIsReversed(prev => !prev);
   };
 
   const reset = () => {
-    setVisibleGoods(goodsFromServer);
     setSortField('');
+    setIsReversed(false);
   };
+
+  const visibleGoods = getVisibleGoods(goodsFromServer, sortField, isReversed);
 
   return (
     <div className="section content">
@@ -74,14 +84,14 @@ export const App = () => {
         <button
           type="button"
           className={classNames('button is-warning', {
-            'is-light': sortField !== SORT_FIELD_REVERSE,
+            'is-light': !isReversed,
           })}
           onClick={reverse}
         >
           Reverse
         </button>
 
-        {sortField && (
+        {(sortField || isReversed) && (
           <button
             type="button"
             className="button is-danger is-light"
