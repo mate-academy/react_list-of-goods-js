a277c382344b43d1c30dc35aa2cf0d77a16c53e7
Melnyk Andriian <andrian_m@ukr.net>
2024-07-29 09:57:52 +0300

    solution was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import cn from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +17,102 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+const BUTTONS = {
+  ALPHABET: 'alphabet',
+  LENGTH: 'length',
+};
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+export const App = () => {
+  const [sortButton, setSortButton] = useState('');
+  const [reversed, setReversed] = useState(false);
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  function prepareList(someList) {
+    let preparedList = [...someList];
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+    switch (sortButton) {
+      case BUTTONS.ALPHABET:
+        preparedList = preparedList.sort((a, b) => {
+          return a.localeCompare(b);
+        });
+        break;
+
+      case BUTTONS.LENGTH:
+        preparedList = preparedList.sort((a, b) => {
+          return a.length - b.length;
+        });
+        break;
+
+      default:
+        return preparedList;
+    }
+
+    return preparedList;
+  }
+
+  function reverseVisibleList(listToReverse, reverseParam) {
+    return reverseParam ? listToReverse.reverse() : listToReverse;
+  }
+
+  const visibleList = prepareList(goodsFromServer);
+  const acltualList = reverseVisibleList(visibleList, reversed);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          onClick={() => setSortButton(BUTTONS.ALPHABET)}
+          type="button"
+          className={cn('button', 'is-info', {
+            'is-light': sortButton !== BUTTONS.ALPHABET,
+          })}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          onClick={() => setSortButton(BUTTONS.LENGTH)}
+          type="button"
+          className={cn('button', 'is-success', {
+            'is-light': sortButton !== BUTTONS.LENGTH,
+          })}
+        >
+          Sort by length
+        </button>
+
+        <button
+          onClick={() => {
+            setReversed(prev => !prev);
+          }}
+          type="button"
+          className={cn('button', 'is-warning', {
+            'is-light': !reversed,
+          })}
+        >
+          Reverse
+        </button>
+
+        {(sortButton || reversed) && (
+          <button
+            onClick={() => {
+              setSortButton('');
+              setReversed(false);
+            }}
+            type="button"
+            className="button is-danger is-light"
+          >
+            Reset
+          </button>
+        )}
+      </div>
+      <ul>
+        {acltualList.map(item => {
+          return (
+            <li key={item} data-cy="Good">
+              {item}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
