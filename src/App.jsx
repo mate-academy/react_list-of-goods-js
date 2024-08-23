c9ccce05f681d4d52599ea87ee68b8d66c7cb232
luter <newgglutt@gmail.com>
2024-08-23 18:42:18 +0300

    sd12s

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -19,22 +20,6 @@ export const App = () => {
   const [sortType, setSortType] = useState(null);
   const [isReversed, setIsReversed] = useState(false);
 
-  const getVisibleGoods = (goods, currentSortType, currentIsReversed) => {
-    const goodsCopy = [...goods];
-
-    if (currentSortType === 'alphabetical') {
-      goodsCopy.sort((a, b) => a.localeCompare(b));
-    } else if (currentSortType === 'byLength') {
-      goodsCopy.sort((a, b) => a.length - b.length);
-    }
-
-    if (currentIsReversed) {
-      goodsCopy.reverse();
-    }
-
-    return goodsCopy;
-  };
-
   const visibleGoods = getVisibleGoods(goodsFromServer, sortType, isReversed);
 
   const resetOrder = () => {
@@ -47,7 +32,7 @@ export const App = () => {
       <div className="buttons">
         <button
           type="button"
-          className={`button is-info ${sortType === 'alphabetical' ? '' : 'is-light'}`}
+          className={classNames('button', 'is-info', { 'is-light': sortType !== 'alphabetical' })}
           onClick={() => setSortType('alphabetical')}
         >
           Sort alphabetically
@@ -55,7 +40,7 @@ export const App = () => {
 
         <button
           type="button"
-          className={`button is-success ${sortType === 'byLength' ? '' : 'is-light'}`}
+          className={classNames('button', 'is-success', { 'is-light': sortType !== 'byLength' })}
           onClick={() => setSortType('byLength')}
         >
           Sort by length
@@ -63,21 +48,20 @@ export const App = () => {
 
         <button
           type="button"
-          className={`button is-warning ${isReversed ? '' : 'is-light'}`}
-          onClick={() => setIsReversed(!isReversed)}
+          className={classNames('button', 'is-warning', { 'is-light': !isReversed })}
+          onClick={() => setIsReversed(prev => !prev)}
         >
           Reverse
         </button>
 
-        {(sortType || isReversed) && (
-          <button
-            type="button"
-            className="button is-danger"
-            onClick={resetOrder}
-          >
-            Reset
-          </button>
-        )}
+        <button
+          type="button"
+          className={classNames('button', 'is-danger', { 'is-light': !sortType && !isReversed })}
+          onClick={resetOrder}
+          disabled={!sortType && !isReversed}
+        >
+          Reset
+        </button>
       </div>
 
       <ul>
@@ -89,4 +73,25 @@ export const App = () => {
       </ul>
     </div>
   );
+};
+
+const getVisibleGoods = (goods, currentSortType, isReversed) => {
+  let goodsCopy = [...goods];
+
+  switch (currentSortType) {
+    case 'alphabetical':
+      goodsCopy.sort((a, b) => a.localeCompare(b));
+      break;
+    case 'byLength':
+      goodsCopy.sort((a, b) => a.length - b.length);
+      break;
+    default:
+      break;
+  }
+
+  if (isReversed) {
+    goodsCopy.reverse();
+  }
+
+  return goodsCopy;
 };
