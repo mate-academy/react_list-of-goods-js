c2e279672b183fb16b49447cf30cc6f77f0fadf8
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-09-20 00:09:54 +0300

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +16,94 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+const SORT_BY_NAME = 'name';
+const SORT_BY_LENGTH = 'length';
+const REVERSE = 'reverse';
+let isNull;
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+export function App() {
+  const goodsCopy = [...goodsFromServer];
+  const [sortType, setSortType] = useState(null);
+  const [reverseOfArray, setReverseOfArray] = useState(null);
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  if (sortType === SORT_BY_NAME) {
+    goodsCopy.sort((goods1, goods2) => goods1.localeCompare(goods2));
+  }
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  if (sortType === SORT_BY_LENGTH) {
+    goodsCopy.sort((goods1, goods2) => goods1.length - goods2.length);
+  }
+
+  if (reverseOfArray === REVERSE) {
+    goodsCopy.reverse();
+  }
+
+  if (sortType === null && reverseOfArray === null) {
+    isNull = null;
+  } else {
+    isNull = 'yes';
+  }
+
+  function setResets() {
+    setSortType(null);
+    setReverseOfArray(null);
+  }
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          onClick={() => setSortType(SORT_BY_NAME)}
+          type="button"
+          className="button is-info is-light"
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          onClick={() => setSortType(SORT_BY_LENGTH)}
+          type="button"
+          className="button is-success is-light"
+        >
+          Sort by length
+        </button>
+
+        {reverseOfArray === null ? (
+          <button
+            onClick={() => setReverseOfArray(REVERSE)}
+            type="button"
+            className="button is-warning is-light"
+          >
+            Reverse
+          </button>
+        ) : (
+          <button
+            onClick={() => setReverseOfArray(null)}
+            type="button"
+            className="button is-warning is-light"
+          >
+            Reverse
+          </button>
+        )}
+
+        {isNull !== null ? (
+          <button
+            onClick={setResets}
+            type="button"
+            className="button is-danger is-light"
+          >
+            Reset
+          </button>
+        ) : (
+          ''
+        )}
+      </div>
+
+      <ul>
+        {goodsCopy.map(good => (
+          <li data-cy="Good">{good}</li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+}
