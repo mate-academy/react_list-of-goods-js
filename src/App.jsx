aab4d9fe7b1206df74185d10eb45ca366f9918cc
Oleksandra Kuziura <radioday2014@gmail.com>
2024-09-27 11:44:43 +0300

    started over again, buttons work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bulma/css/bulma.css';
+import { useState } from 'react';
 import './App.scss';
 
 export const goodsFromServer = [
@@ -13,34 +14,87 @@ export const goodsFromServer = [
   'Jam',
   'Garlic',
 ];
+const STATE_ALPHABETICALLY = 'alpha';
+const STATE_BY_LENGTH = 'Length';
+const STATE_REVERSE = 'reverse';
+const STATE_RESET = 'reset';
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+function sorting(list, state) {
+  const listForSorting = [...list];
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  if (state === STATE_ALPHABETICALLY) {
+    return listForSorting.sort((item1, item2) => item1.localeCompare(item2));
+  }
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  if (state === STATE_BY_LENGTH) {
+    return listForSorting.sort((item1, item2) => item1.length - item2.length);
+  }
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  if (state === STATE_REVERSE) {
+    return listForSorting.reverse();
+  }
+
+  return list;
+}
+
+export const App = () => {
+  const [state, setState] = useState(null);
+  const [listOfGoods, setListOfGoods] = useState(goodsFromServer);
+  const listForPrint = sorting(listOfGoods, state);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className="button is-info is-light"
+          onClick={() => {
+            setState(STATE_ALPHABETICALLY);
+          }}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className="button is-success is-light"
+          onClick={() => {
+            setState(STATE_BY_LENGTH);
+          }}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className="button is-warning is-light"
+          onClick={() => {
+            setState(STATE_REVERSE);
+            setListOfGoods(listForPrint);
+          }}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          className="button is-danger is-light"
+          onClick={() => {
+            setState(STATE_RESET);
+            setListOfGoods(goodsFromServer);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+
+      <ul>
+        {listForPrint.map(good => (
+          <li data-cy="Good" key={good}>
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
