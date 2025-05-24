fdcf9e4465709e6f11b213c667715626a26f54a7
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-05-24 21:53:51 +0100

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bulma/css/bulma.css';
+import classNames from 'classnames';
 import './App.scss';
 import { useState } from 'react';
 
@@ -17,39 +18,37 @@ export const goodsFromServer = [
 
 export const App = () => {
   const [goods, setGood] = useState(goodsFromServer);
-  const [state, setState] = useState({ key: null, direction: null });
+  const [state, setState] = useState('noReset');
 
   const sortByLenght = () => {
-    setGood([...goods].sort((a, b) => a.length - b.length));
-    setState({ key: 'length', direction: 'asc' });
-  };
+    if (state !== 'length') {
+      setGood([...goods].sort((a, b) => a.length - b.length));
+      setState('length');
+    } else {
+      setGood([...goods].sort((a, b) => b.length - a.length));
+      setState(null);
+    }
+  }
 
   const sortByAlphabet = () => {
-    setGood([...goods].sort((a, b) => a.localeCompare(b)));
-    setState({ key: 'alphabet', direction: 'asc' });
-  };
-
-  const reversed = () => {
-    if (state.key === 'length' && state.direction === 'asc') {
-      state.direction = 'desc';
-      setGood([...goods].sort((a, b) => b.length - a.length));
-    } else if (state.key === 'length' && state.direction === 'desc') {
-      state.direction = 'asc';
-      setGood([...goods].sort((a, b) => a.length - b.length));
-    } else if (state.key === 'alphabet' && state.direction === 'asc') {
-      state.direction = 'desc';
-      setGood([...goods].sort((a, b) => b.localeCompare(a)));
-    } else if (state.key === 'alphabet' && state.direction === 'desc') {
-      state.direction = 'asc';
+    if (state !== 'alphabet') {
       setGood([...goods].sort((a, b) => a.localeCompare(b)));
+      setState('alphabet');
     } else {
-      setGood(
-        [...goods].reverse()
-      )
+      setGood([...goods].sort((a, b) => b.localeCompare(a)));
+      setState(null);
     }
   };
 
-  const reset = () => setGood([...goodsFromServer]);
+  const reversed = () => {
+    setGood([...goods].reverse());
+    setState('reversed');
+  };
+
+  const reset = () => {
+    setGood([...goodsFromServer]);
+    setState('noReset');
+  };
 
   return (
     <div className="section content">
@@ -57,30 +56,42 @@ export const App = () => {
         <button
           onClick={sortByAlphabet}
           type="button"
-          className="button is-info is-light"
+          className={classNames('button is-success', {
+            'is-light': state !== 'alphabet',
+          })}
         >
           Sort alphabetically
         </button>
-
         <button
           onClick={sortByLenght}
           type="button"
-          className="button is-success is-light"
+          className={classNames('button is-success', {
+            'is-light': state !== 'length',
+          })}
         >
           Sort by length
         </button>
-
         <button
           onClick={reversed}
           type="button"
-          className="button is-warning is-light"
+          className={classNames('button is-warning', {
+            'is-light': state !== 'reversed',
+          })}
         >
           Reverse
         </button>
 
-        <button onClick={reset} type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {state !== 'noReset' && (
+          <button
+            onClick={reset}
+            type="button"
+            className={classNames('button is-danger', {
+              'is-light': state !== 'noReset',
+            })}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
