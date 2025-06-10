43eff104fef34d8b47bc810920d23603b3940a10
petrushinsky-kirill <petrushinsky@gmail.com>
2025-06-10 14:12:51 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +15,74 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+const arraysAreEqual = (a, b) => {
+  return a.length === b.length && a.every((item, i) => item === b[i]);
+};
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+export const App = () => {
+  const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
+  const [isActive, setActive] = useState('');
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  const sortAlph = () => {
+    setVisibleGoods([...visibleGoods].sort((a, b) => a.localeCompare(b)));
+    setActive('alph');
+  };
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  const sortLength = () => {
+    setVisibleGoods([...visibleGoods].sort((a, b) => a.length - b.length));
+    setActive('length');
+  };
+
+  const reverse = () => {
+    setVisibleGoods([...visibleGoods].reverse());
+    setActive('reverse');
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info${isActive !== 'alph' ? ' is-light' : ''}`}
+          onClick={sortAlph}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={`button is-info${isActive !== 'length' ? ' is-light' : ''}`}
+          onClick={sortLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={`button is-info${isActive !== 'reverse' ? ' is-light' : ''}`}
+          onClick={reverse}
+        >
+          Reverse
+        </button>
+
+        {!arraysAreEqual(visibleGoods, goodsFromServer) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => setVisibleGoods(goodsFromServer)}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
