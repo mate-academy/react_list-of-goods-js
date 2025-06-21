ca78204a6a581134da9ac197e53e3a735831207c
Svystun Pavlo <spo26081985@gmail.com>
2025-06-21 22:07:45 +0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,22 @@ export const goodsFromServer = [
   'Honey',
   'Jam',
   'Garlic',
-]
+];
 
-const SORT_FIELD_LENGTH = 'good';
-const SORT_FIELD_SUCCESS = 'success';
+const SORT_FIELD_LENGTH = 'length';
+const SORT_FIELD_SUCCESS = 'alphabetical';
 const SORT_REVERSE = 'reversed';
 
-function getPreparedGoods(goods, {sortFiled, reversed} ) {
-  let preparedGoods = [...goods];
+function getPreparedGoods(goods, { sortFiled, reversed }) {
+  const preparedGoods = [...goods];
 
-  if(sortFiled) {
+  if (sortFiled) {
     preparedGoods.sort((good1, good2) => {
-      switch(sortFiled) {
+      switch (sortFiled) {
         case SORT_FIELD_SUCCESS:
           return good1.localeCompare(good2);
         case SORT_FIELD_LENGTH:
-          return good2.length - good1.length;
+          return good1.length - good2.length;
 
         default:
           return 0;
@@ -36,7 +36,7 @@ function getPreparedGoods(goods, {sortFiled, reversed} ) {
     });
   }
 
-   if (reversed) {
+  if (reversed) {
     switch (reversed) {
       case SORT_REVERSE:
         return preparedGoods.reverse();
@@ -51,23 +51,33 @@ function getPreparedGoods(goods, {sortFiled, reversed} ) {
 export const App = () => {
   const [sortFiled, setSortFiled] = useState('');
   const [reversed, setReversed] = useState('');
-  const visibleGoods = getPreparedGoods(goodsFromServer, {sortFiled, reversed});
+  const visibleGoods = getPreparedGoods(goodsFromServer, {
+    sortFiled,
+    reversed,
+  });
 
-
-    return (
+  return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className={`button is-success ${sortFiled === SORT_FIELD_SUCCESS ? '' : 'is-light'}`}
-          onClick={() => setSortFiled(SORT_FIELD_SUCCESS)}>
+        <button
+          type="button"
+          className={`button is-success ${sortFiled === SORT_FIELD_SUCCESS ? '' : 'is-light'}`}
+          onClick={() => setSortFiled(SORT_FIELD_SUCCESS)}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className={`button is-success ${sortFiled === SORT_FIELD_LENGTH ? '' : 'is-light'}`}
-          onClick={() => setSortFiled(SORT_FIELD_LENGTH)}>
+        <button
+          type="button"
+          className={`button is-success ${sortFiled === SORT_FIELD_LENGTH ? '' : 'is-light'}`}
+          onClick={() => setSortFiled(SORT_FIELD_LENGTH)}
+        >
           Sort by length
         </button>
 
-        <button type="button" className={`button is-warning ${reversed === SORT_REVERSE ? '' : 'is-light'}`}
+        <button
+          type="button"
+          className={`button is-warning ${reversed === SORT_REVERSE ? '' : 'is-light'}`}
           onClick={() => {
             if (reversed === '') {
               setReversed(SORT_REVERSE);
@@ -79,25 +89,27 @@ export const App = () => {
           Reverse
         </button>
 
-         {(sortFiled !== '' || reversed !== '') && (
-        <button type="button" className="button is-danger is-light"
-          onClick={() => {
-            setSortFiled('');
-            setReversed('')
-          }}
-        >
-          Reset
-        </button>
-         )}
+        {(sortFiled !== '' || reversed !== '') && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setSortFiled('');
+              setReversed('');
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         {visibleGoods.map(good => (
-        <li key={good} data-cy="Good">
-          {good}
-        </li>
-      ))}
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
-}
+};
