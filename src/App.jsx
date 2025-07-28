b1eee89d5308372059e1cc2cfefb5e1e9978c0a3
Vitalii Kredinets <kredinec.v@gmail.com>
2025-07-28 16:21:10 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +16,87 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [activeButton, setActiveButton] = useState(null);
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  const NOT_ACTIVE_CLASS = 'is-light';
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  const isOriginalOrder = () => {
+    return goods.every((good, index) => good === goodsFromServer[index]);
+  };
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  const sortByAlphabet = () => {
+    setGoods([...goods].slice().sort());
+    setActiveButton('alphabet');
+  };
+
+  const sortByLength = () => {
+    setGoods([...goods].slice().sort((a, b) => a.length - b.length));
+    setActiveButton('length');
+  };
+
+  const reversedGoods = () => {
+    setGoods([...goods].slice().reverse());
+    setActiveButton('reverse');
+  };
+
+  const resetGoods = () => {
+    setGoods([...goodsFromServer]);
+    setActiveButton(null);
+  };
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={classNames('button', 'is-info', {
+            [NOT_ACTIVE_CLASS]: activeButton !== 'alphabet',
+          })}
+          onClick={sortByAlphabet}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={classNames('button', 'is-success', {
+            [NOT_ACTIVE_CLASS]: activeButton !== 'length',
+          })}
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={classNames('button', 'is-warning', {
+            [NOT_ACTIVE_CLASS]: activeButton !== 'reverse',
+          })}
+          onClick={reversedGoods}
+        >
+          Reverse
+        </button>
+
+        {!isOriginalOrder() && (
+          <button
+            type="button"
+            className={classNames('button', 'is-danger', NOT_ACTIVE_CLASS)}
+            onClick={resetGoods}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
