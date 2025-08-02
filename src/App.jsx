712ba75971a2d2ae3c2eb5045139d0e8b9f53251
nesms <serhiy.nesmeyanov@qpard.com>
2025-08-02 23:57:25 +0300

    feat: create task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +16,112 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [selectedFilter, setSelectedFilter] = useState(null);
+  const [reverse, setReverse] = useState(false);
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  const sortAlphabet = () => {
+    const sorted = [...goodsFromServer].sort();
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+    if (reverse) {
+      sorted.reverse();
+    }
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+    setGoods(sorted);
+  };
+
+  const sortByLength = () => {
+    setGoods(prev => {
+      const tmp = [...prev].sort((a, b) => {
+        if (a.length === b.length) {
+          return a.localeCompare(b);
+        }
+
+        return a.length - b.length;
+      });
+
+      if (reverse) {
+        tmp.reverse();
+      }
+
+      return tmp;
+    });
+  };
+
+  useEffect(() => {
+    switch (selectedFilter) {
+      case 'alphabetically':
+        sortAlphabet();
+        break;
+      case 'length':
+        sortByLength();
+        break;
+      default:
+        if (reverse) {
+          setGoods([...goodsFromServer].reverse());
+          break;
+        }
+
+        setGoods([...goodsFromServer]);
+        break;
+    }
+  }, [selectedFilter, reverse]);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={classNames('button is-info', {
+            'is-light': selectedFilter !== 'alphabetically',
+          })}
+          onClick={() => setSelectedFilter('alphabetically')}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={classNames('button is-success', {
+            'is-light': selectedFilter !== 'length',
+          })}
+          onClick={() => setSelectedFilter('length')}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={classNames('button is-warning ', {
+            'is-light': !reverse,
+          })}
+          onClick={() => setReverse(!reverse)}
+        >
+          Reverse
+        </button>
+
+        {(selectedFilter || reverse) && (
+          <button
+            type="button"
+            className="button is-danger"
+            onClick={() => {
+              setSelectedFilter(null);
+              setReverse(false);
+            }}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goods.map(item => (
+          <li data-cy="Good" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
