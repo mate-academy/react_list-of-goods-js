7e00836279756bba285eb184336968401a745a9e
Leonardo Espinel <leo15782@gmail.com>
2025-09-26 21:32:44 -0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +15,107 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [isReversed, setIsReversed] = useState(false);
+  const [isSortByLength, setIsSortByLength] = useState(false);
+  const [isSortByAlphabetically, setIsSortByAlphabetically] = useState(false);
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  const sortAlphabetically = () => {
+    const sorted = [...goodsFromServer].sort((a, b) => a.localeCompare(b));
+    const finalGoods = isReversed ? sorted.reverse() : sorted;
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+    setGoods(finalGoods);
+    setIsSortByLength(false);
+    setIsSortByAlphabetically(true);
+  };
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  const sortByLength = () => {
+    const sorted = [...goodsFromServer].sort((a, b) => a.length - b.length);
+    const finalGoods = isReversed ? sorted.reverse() : sorted;
+
+    setGoods(finalGoods);
+    setIsSortByLength(true);
+    setIsSortByAlphabetically(false);
+  };
+
+  const reverse = () => {
+    if (isSortByAlphabetically) {
+      const sorted = [...goodsFromServer].sort((a, b) => a.localeCompare(b));
+      const finalGoods = isReversed ? sorted : sorted.reverse();
+
+      setGoods(finalGoods);
+    } else if (isSortByLength) {
+      const sorted = [...goodsFromServer].sort((a, b) => a.length - b.length);
+      const finalGoods = isReversed ? sorted : sorted.reverse();
+
+      setGoods(finalGoods);
+    } else {
+      const finalGoods = isReversed
+        ? goodsFromServer
+        : [...goodsFromServer].reverse();
+
+      setGoods(finalGoods);
+    }
+
+    setIsReversed(!isReversed);
+  };
+
+  const reset = () => {
+    setGoods(goodsFromServer);
+    setIsReversed(false);
+    setIsSortByLength(false);
+    setIsSortByAlphabetically(false);
+  };
+
+  const isOriginalOrder =
+    JSON.stringify(goods) === JSON.stringify(goodsFromServer);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${isSortByAlphabetically ? '' : 'is-light'}`}
+          onClick={sortAlphabetically}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={`button is-success ${isSortByLength ? '' : 'is-light'}`}
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={`button is-warning ${isReversed ? '' : 'is-light'}`}
+          onClick={reverse}
+        >
+          Reverse
+        </button>
+
+        {!isOriginalOrder && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
