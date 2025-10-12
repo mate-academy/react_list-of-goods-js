597e41455d0c05dc381d115a0746aa5a1adcb654
R1chardD0e <doerichard190@gmail.com>
2025-10-12 18:55:10 +0200

    Implement sorting functionality for goods with buttons and reset option

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +15,125 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const sortList = [
+  {
+    id: 1,
+    title: 'Sort alphabetically',
+    className: 'is-info',
+  },
+  {
+    id: 2,
+    title: 'Sort by length',
+    className: 'is-success',
+  },
+  {
+    id: 3,
+    title: 'Reverse',
+    className: 'is-warning',
+  },
+];
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+export const App = () => {
+  const [sortField, setSortField] = useState(null);
+  const [isReverse, setIsReverse] = useState(false);
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  const visibleList = goodsFromServer.toSorted((a, b) => {
+    switch (sortField) {
+      case 'Sort alphabetically':
+        return a.localeCompare(b);
+      case 'Sort by length':
+        return a.length - b.length;
+      default:
+        return 0;
+    }
+  });
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  if (isReverse) {
+    visibleList.reverse();
+  }
+
+  const handleSort = field => {
+    switch (field) {
+      case 'Reverse':
+        setIsReverse(!isReverse);
+        break;
+      case 'Reset':
+        setSortField(null);
+        setIsReverse(false);
+        break;
+      default:
+        setSortField(field);
+    }
+  };
+
+  return (
+    <div className="section content">
+      <div className="section content">
+        {sortList.map(field => (
+          <button
+            key={field.id}
+            type="button"
+            className={`button ${field.className} ${
+              field.title === sortField ||
+              (field.title === 'Reverse' && isReverse)
+                ? ''
+                : 'is-light'
+            }`}
+            onClick={() => handleSort(field.title)}
+          >
+            {field.title}
+          </button>
+        ))}
+
+        {(sortField || isReverse) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => handleSort('Reset')}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {visibleList.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
+  );
 
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  /*  return (
+    <div className="section content">
+      <div className="buttons">
+        <button type="button" className="button is-info is-light">
+          Sort alphabetically
+        </button>
+
+        <button type="button" className="button is-success is-light">
+          Sort by length
+        </button>
+
+        <button type="button" className="button is-warning is-light">
+          Reverse
+        </button>
+
+        <button type="button" className="button is-danger is-light">
+          Reset
+        </button>
+      </div>
+
+      <ul>
+        <li data-cy="Good">Dumplings</li>
+        <li data-cy="Good">Carrot</li>
+        <li data-cy="Good">Eggs</li>
+        <li data-cy="Good">Ice cream</li>
+        <li data-cy="Good">Apple</li>
+        <li data-cy="Good">...</li>
+      </ul>
+    </div>
+  );  */
+};
