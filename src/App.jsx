def90a7aa245b03538647ae6dcf7ba35655452e4
Hannah Olbrich <hannaholbrich@gmail.com>
2025-12-20 16:55:17 +0000

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -19,65 +19,91 @@ export const App = () => {
  const [goods, setGoods] = useState(goodsFromServer);
   const [isModified, setIsModified] = useState(false);
   const [isReversed, setIsReversed] = useState(false);
+  const [sortType, setSortType] = useState('');
 
   const sortAlphabetically = () => {
-    setGoods([...goods].sort((a, b) => a.localeCompare(b)));
+    const sorted = [...goodsFromServer].sort((a, b) => a.localeCompare(b));
+    setGoods(isReversed ? sorted.reverse() : sorted);
     setIsModified(true);
-    setIsReversed(false);
+    setSortType('alphabetically');
   };
 
   const sortByLength = () => {
-    setGoods([...goods].sort((a, b) => a.length - b.length));
+    const sorted = [...goodsFromServer].sort((a, b) => a.length - b.length);
+    setGoods(isReversed ? sorted.reverse() : sorted);
     setIsModified(true);
-    setIsReversed(false);
+    setSortType('length');
   };
 
-  const reverseGoods = () => {
-    setGoods([...goods].reverse());
-    setIsModified(true);
-    setIsReversed(!isReversed);
+ const reverseGoods = () => {
+    const reversed = [...goods].reverse();
+    setGoods(reversed);
+    const newIsReversed = !isReversed;
+    setIsReversed(newIsReversed);
+
+    // Check if we're back to original order
+    const isBackToOriginal = reversed.every((item, index) => item === goodsFromServer[index]);
+    setIsModified(!isBackToOriginal || sortType !== '');
   };
 
   const resetGoods = () => {
     setGoods(goodsFromServer);
     setIsModified(false);
     setIsReversed(false);
+    setSortType('');
   };
 
-
   return (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light" onClick={sortAlphabetically}>
-        Sort alphabetically
-      </button>
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${sortType === 'alphabetically' ? '' : 'is-light'}`}
+          onClick={sortAlphabetically}
+        >
+          Sort alphabetically
+        </button>
 
-      <button type="button" className="button is-success is-light" onClick={sortByLength}>
-        Sort by length
-      </button>
+        <button
+          type="button"
+          className={`button is-success ${sortType === 'length' ? '' : 'is-light'}`}
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
 
-      <button type="button" className= {`button is-warning ${isReversed ? 'is-warning' : 'is-light' }`} onClick={reverseGoods}>
-        Reverse
-      </button>
+        <button
+          type="button"
+          className={`button is-warning ${isReversed ? '' : 'is-light'}`}
+          onClick={reverseGoods}
+        >
+          Reverse
+        </button>
 
-      { isModified && (
-      <button type="button" className="button is-danger is-light" onClick={resetGoods}>
-        Reset
-      </button>
-      )}
-    </div>
-  <div className = "goods-list">
-    { goods.length === 0 && (
-      <div data-cy="NoGoods" className="notification is-warning">
-        No goods available
+        {isModified && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={resetGoods}
+          >
+            Reset
+          </button>
+        )}
       </div>
-    )}
-    <ul>
-      {goods.map((good) => (
-        <li key={good} data-cy="Good">{good}</li>
-      ))}
-    </ul>
-  </div>
-</div>
+      <div className="goods-list">
+        {goods.length === 0 && (
+          <div data-cy="NoGoods" className="notification is-warning">
+            No goods available
+          </div>
+        )}
+        <ul>
+          {goods.map(good => (
+            <li key={good} data-cy="Good">
+              {good}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
