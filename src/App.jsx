0a2ef2684a22f4a5d130c37e8ddbfb9022eafdb0
Hannah Olbrich <hannaholbrich@gmail.com>
2025-12-20 16:47:35 +0000

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +15,69 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
+export const App = () => {
+ const [goods, setGoods] = useState(goodsFromServer);
+  const [isModified, setIsModified] = useState(false);
+  const [isReversed, setIsReversed] = useState(false);
+
+  const sortAlphabetically = () => {
+    setGoods([...goods].sort((a, b) => a.localeCompare(b)));
+    setIsModified(true);
+    setIsReversed(false);
+  };
+
+  const sortByLength = () => {
+    setGoods([...goods].sort((a, b) => a.length - b.length));
+    setIsModified(true);
+    setIsReversed(false);
+  };
+
+  const reverseGoods = () => {
+    setGoods([...goods].reverse());
+    setIsModified(true);
+    setIsReversed(!isReversed);
+  };
+
+  const resetGoods = () => {
+    setGoods(goodsFromServer);
+    setIsModified(false);
+    setIsReversed(false);
+  };
+
+
+  return (
   <div className="section content">
     <div className="buttons">
-      <button type="button" className="button is-info is-light">
+      <button type="button" className="button is-info is-light" onClick={sortAlphabetically}>
         Sort alphabetically
       </button>
 
-      <button type="button" className="button is-success is-light">
+      <button type="button" className="button is-success is-light" onClick={sortByLength}>
         Sort by length
       </button>
 
-      <button type="button" className="button is-warning is-light">
+      <button type="button" className= {`button is-warning ${isReversed ? 'is-warning' : 'is-light' }`} onClick={reverseGoods}>
         Reverse
       </button>
 
-      <button type="button" className="button is-danger is-light">
+      { isModified && (
+      <button type="button" className="button is-danger is-light" onClick={resetGoods}>
         Reset
       </button>
+      )}
     </div>
-
+  <div className = "goods-list">
+    { goods.length === 0 && (
+      <div data-cy="NoGoods" className="notification is-warning">
+        No goods available
+      </div>
+    )}
     <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
+      {goods.map((good) => (
+        <li key={good} data-cy="Good">{good}</li>
+      ))}
     </ul>
   </div>
-);
+</div>
+  );
+};
