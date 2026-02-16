ea52818b4fa35b1c5432769bb993c49890e29a61
Boris Mund <borismund@Mac.lan>
2026-02-16 07:40:59 +0200

    react_sort-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +16,86 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+const SORT_FIELD_ALPHA = "alpha"
+const SORT_FIELD_LEN = "length"
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
-    </div>
+export const App = () => {
 
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  const [sortField, setSortField] = useState("")
+  const [sortRev, setSortRev] = useState(false);
+
+  const handleReset = () => {
+    setSortField("");
+    setSortRev(false);
+  }
+  let arrCopy = [...goodsFromServer];
+
+  if (sortField === SORT_FIELD_ALPHA) {
+    arrCopy.sort((a, b) =>
+      a.localeCompare(b))
+  }
+  if (sortField === SORT_FIELD_LEN) {
+    arrCopy.sort((a, b) =>
+      a.length - b.length)
+  }
+  if (sortRev) {
+    arrCopy.reverse();
+  }
+
+  return (
+
+    <div className="section content">
+      <div className="buttons">
+        <button onClick={() => setSortField(SORT_FIELD_ALPHA)}
+          type="button"
+          className={cn(
+            "button is-info",
+            { "is-light": sortField !== SORT_FIELD_ALPHA }
+          )}
+        >
+          Sort alphabetically
+        </button>
+
+        <button onClick={() => setSortField(SORT_FIELD_LEN)}
+          type="button"
+          className={cn(
+            "button is-success",
+            { "is-light": sortField !== SORT_FIELD_LEN }
+          )}
+        >
+          Sort by length
+        </button>
+
+        <button onClick={() => setSortRev(prev => !prev)}
+          type="button"
+          className={cn(
+            "button is-warning",
+            { "is-light": !sortRev }
+          )}
+        >
+          Reverse
+        </button>
+
+        {(sortField !== "" || sortRev) && (
+          <button onClick={handleReset} type="button" className=
+            "button is-danger is-light"
+
+          >
+            Reset
+          </button>
+        )}
+      </div >
+
+      <ul> {
+        arrCopy.map(good => {
+          return (
+            <li data-cy="Good" key={good} >{good}</li>
+          )
+        })
+      }
+      </ul>
+    </div >
+  )
+}
