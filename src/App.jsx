98d0a3300a7c2bde550ba0a8498686493478f181
Dima Sidenko <sidenkodima2202@gmail.com>
2026-02-24 13:28:52 +0100

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -87,13 +87,15 @@ export const App = () => {
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-          onClick={reset}
-        >
-          Reset
-        </button>
+        {(sortField || reversed) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
