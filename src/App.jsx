86535cf91a5bb8c94c1e880e25c73897970df0f4
Valentyna <valya0322@ukr.net>
2026-05-10 12:10:44 +0300

    Added sorting functionality: alphabetical, by length, reverse, and a reset all option.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+// import cn from "classnames";
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +17,124 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+const SORT_ALPHABETICALLY = 'alphabetically';
+// const SORT_LENGTH = 'name';
+// const SORT_FIELD_COLOR = 'color';
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+function getPreparedGoods(goods, { sortAlphabetically, reversed, length }) {
+  let preperdGoods = [...goods];
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  if (sortAlphabetically) {
+    preperdGoods.sort((good1, good2) => good1.localeCompare(good2));
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+    // {
+    //   switch (sortAlphabetically) {
+    //     case SORT_ALPHABETICALLY:
+    //       return good1 - good2;
+
+    //     case SORT_FIELD_NAME:
+    //     case SORT_FIELD_COLOR:
+    //       return good1.localeCompare(good2);
+    //     default:
+    //       return 0;
+    //   }
+    // });
+  }
+
+  if (length) {
+    preperdGoods = preperdGoods.sort(
+      (good1, good2) => good1.length - good2.length,
+    );
+  }
+
+  if (reversed) {
+    preperdGoods = preperdGoods.toReversed();
+  }
+
+  return preperdGoods;
+}
+
+export const App = () => {
+  const [sortAlphabetically, setSortAlphabetically] = useState('');
+  const [reversed, setReversed] = useState(false);
+  const [length, setLength] = useState(0);
+
+  const visibleGoods = getPreparedGoods(goodsFromServer, {
+    sortAlphabetically,
+    length,
+    reversed,
+  });
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          // className="button is-info is-light"
+          className={`button is-info ${
+            sortAlphabetically === SORT_ALPHABETICALLY ? '' : 'is-light'
+          }`}
+          onClick={() => setSortAlphabetically(SORT_ALPHABETICALLY)}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          // className="button is-success is-light"
+          className={`button is-success ${
+            length === SORT_ALPHABETICALLY ? '' : 'is-light'
+          }`}
+          onClick={() => setLength(SORT_ALPHABETICALLY)}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          // className="button is-warning is-light"
+          className={`button is-warning ${
+            reversed === false ? 'is-light' : ''
+          }`}
+          onClick={() => setReversed(!reversed)}
+        >
+          Reverse
+        </button>
+
+        {(sortAlphabetically || length || reversed) && (
+          <button
+            type="button"
+            // className="button is-danger is-light"
+            // className="button is-danger"
+            className={`button is-danger
+            ${sortAlphabetically === SORT_ALPHABETICALLY ? '' : 'is-light'}`}
+            onClick={() => {
+              setSortAlphabetically('');
+              setLength('');
+              setReversed(false);
+            }}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
+
+      {/* <ul>
+        <li data-cy="Good">Dumplings</li>
+        <li data-cy="Good">Carrot</li>
+        <li data-cy="Good">Eggs</li>
+        <li data-cy="Good">Ice cream</li>
+        <li data-cy="Good">Apple</li>
+        <li data-cy="Good">...</li>
+      </ul> */}
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
