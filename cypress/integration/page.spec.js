e8cd1b4763ed0bbd6ab1b2a410793d2720be280b
Maksym Savchenko <maks.20050817@gmail.com>
2026-06-25 14:09:15 +0200

    ready to check

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -23,8 +23,6 @@ Cypress.on('fail', (e) => {
 
 describe('Page', () => {
   beforeEach(() => {
-    if (failed) Cypress.runner.stop();
-
     cy.visit('/');
   });
 
@@ -194,7 +192,7 @@ describe('Page', () => {
 
     it('should allow to reverse again', () => {
       page.reverseButton().click();
-      
+
       page.assertFirstGoods('Apple', 'Bread', 'Carrot');
     });
 
@@ -273,7 +271,7 @@ describe('Page', () => {
 
     it('should allow to reverse again', () => {
       page.reverseButton().click();
-      
+
       page.assertFirstGoods('Jam', 'Eggs', 'Fish');
     });
 
