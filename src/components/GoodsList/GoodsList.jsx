2adfe39a1a7ee693af5a874a57910b2ae54c2bec
Skoulskiy <ogurethcs@gmail.com>
2026-07-10 11:11:42 +0300

    added fixes

--- src/components/GoodsList/GoodsList.jsx
+++ src/components/GoodsList/GoodsList.jsx
@@ -3,8 +3,8 @@ import { GoodsItem } from '../GoodsItem/GoodsItem';
 export const GoodsList = ({ goods }) => {
   return (
     <ul>
-      {goods.map((good) => (
-        <GoodsItem good={good} key={Math.floor(Math.random() * 10000)} />
+      {goods.map(good => (
+        <GoodsItem good={good} key={good} />
       ))}
     </ul>
   );
