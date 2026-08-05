3806b09197683ed27765386e3ed53584b62f68a3
Mariana <zh.yu.st22@gmail.com>
2026-08-05 23:43:14 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import cn from 'classnames';
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +16,109 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+const SORT_BY_ALPHABET = 'alphabet';
+const SORT_BY_LENGTH = 'length';
+const REVERSE_LIST = 'reverse';
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+function sortGoodsBy(goods, sortingType) {
+  const sameGoods = [...goods];
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  switch (sortingType) {
+    case SORT_BY_ALPHABET:
+      return sameGoods.sort((good1, good2) => {
+        return good1.localeCompare(good2);
+      });
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+    case SORT_BY_LENGTH:
+      return sameGoods.sort((good1, good2) => {
+        return good1.length - good2.length;
+      });
+
+    case REVERSE_LIST:
+      return sameGoods.reverse();
+
+    default:
+      return goods;
+  }
+}
+
+// eslint-disable-next-line no-fallthrough
+export const App = () => {
+  const [goodsList, setGoodsList] = useState(goodsFromServer);
+  const [currentSorting, setCurrentSorting] = useState('');
+  const [isReversed, setIsReversed] = useState(false);
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn('button', 'is-info', {
+            'is-light': currentSorting !== SORT_BY_ALPHABET,
+          })}
+          onClick={() => {
+            setGoodsList(sortGoodsBy(goodsList, SORT_BY_ALPHABET));
+            setCurrentSorting(SORT_BY_ALPHABET);
+            setIsReversed(false);
+          }}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-success', {
+            'is-light': currentSorting !== SORT_BY_LENGTH,
+          })}
+          onClick={() => {
+            setGoodsList(sortGoodsBy(goodsList, SORT_BY_LENGTH));
+            setCurrentSorting(SORT_BY_LENGTH);
+            setIsReversed(false);
+          }}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={cn('button', 'is-warning', {
+            'is-light': currentSorting !== REVERSE_LIST,
+          })}
+          onClick={() => {
+            setGoodsList(sortGoodsBy(goodsList, REVERSE_LIST));
+
+            setIsReversed(!isReversed);
+
+            setCurrentSorting(isReversed ? '' : REVERSE_LIST);
+          }}
+        >
+          Reverse
+        </button>
+
+        {JSON.stringify(goodsList) !== JSON.stringify(goodsFromServer) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setGoodsList(goodsFromServer);
+              setCurrentSorting('');
+              setIsReversed(false);
+            }}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goodsList.map(good => {
+          return (
+            <li key={good} data-cy="Good">
+              {good}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
