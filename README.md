ef9552d95ed7beb0efdd2cfe57839ebf010d409f
Artem Mayboroda <artmay77777@gmail.com>
2026-08-30 15:43:39 +0300

    Implement goods list sorting

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Implement the ability to reorder the goods with the given buttons.
 
 > Here is [the working version](https://mate-academy.github.io/react_list-of-goods/)
 
+> [DEMO LINK](https://ArtMay777.github.io/react_list-of-goods-js/)
+
 1. `Sort alphabetically` button should sort the goods alphabetically.
     - remove `is-light` class from the button when this order is applied;
 1. `Sort by length` button should sort the goods by length.
@@ -16,7 +18,7 @@ Implement the ability to reorder the goods with the given buttons.
     - it should be visible only if goods are not already in the original order;
 
 ## Instructions
-- Install Prettier Extention and use this [VSCode settings](https://mate-academy.github.io/fe-program/tools/vscode/settings.json) to enable format on save.
+- Install the Prettier extension and use these [VSCode settings](https://mate-academy.github.io/fe-program/tools/vscode/settings.json) to enable format on save.
 - Implement a solution following the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline).
 - Open one more terminal and run tests with `npm test` to ensure your solution is correct.
-- Replace `<your_account>` with your Github username in the [DEMO LINK](https://<your_account>.github.io/react_list-of-goods-js/) and add it to the PR description.
+- Add the [DEMO LINK](https://ArtMay777.github.io/react_list-of-goods-js/) to the PR description.
--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,108 @@
-import 'bulma/css/bulma.css';
-import './App.scss';
+import { useState } from "react";
+import cn from "classnames";
+
+import "bulma/css/bulma.css";
+import "./App.scss";
 
 export const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
+  "Dumplings",
+  "Carrot",
+  "Eggs",
+  "Ice cream",
+  "Apple",
+  "Bread",
+  "Fish",
+  "Honey",
+  "Jam",
+  "Garlic",
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [sortType, setSortType] = useState("");
+  const [isReversed, setIsReversed] = useState(false);
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  const sortAlphabetically = () => {
+    setSortType("alphabetically");
+  };
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  const sortByLength = () => {
+    setSortType("length");
+  };
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  const reverseGoods = () => {
+    setIsReversed((currentValue) => !currentValue);
+  };
+
+  const reset = () => {
+    setSortType("");
+    setIsReversed(false);
+  };
+
+  const visibleGoods = [...goodsFromServer];
+
+  if (sortType === "alphabetically") {
+    visibleGoods.sort((first, second) => first.localeCompare(second));
+  }
+
+  if (sortType === "length") {
+    visibleGoods.sort((first, second) => first.length - second.length);
+  }
+
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn("button is-info", {
+            "is-light": sortType !== "alphabetically",
+          })}
+          onClick={sortAlphabetically}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={cn("button is-success", {
+            "is-light": sortType !== "length",
+          })}
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={cn("button is-warning", {
+            "is-light": !isReversed,
+          })}
+          onClick={reverseGoods}
+        >
+          Reverse
+        </button>
+
+        {(sortType !== "" || isReversed) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {visibleGoods.map((good) => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
